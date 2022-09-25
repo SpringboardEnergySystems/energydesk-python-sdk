7430d7e2a326bd22f7faee06560eb6df30869d7c
Steinar E <s.r.eiksen@gmail.com>
2022-09-25 02:00:44 +0200

    Fixed API error

--- energydeskapi/conversions/elvizlink_api.py
+++ energydeskapi/conversions/elvizlink_api.py
@@ -3,27 +3,21 @@ import json
 import logging
 import pandas as pd
 from energydeskapi.customers.customers_api import CustomersApi
-
+from energydeskapi.customers.users_api import UsersApi
 logger = logging.getLogger(__name__)
 #  Change
 class ElvizLinksApi:
     """Description...
 
       """
-
     @staticmethod
-    def get_treasury_banks(api_connection):
-        logger.info("Fetching treasury bank list")
-        json_res = api_connection.exec_get_url('/api/treasury/treasurybanks/')
-        print(json_res)
-        if json_res is None:
-            return None
-        df = pd.DataFrame(data=json_res)
-        return df
+    def get_company_mappings(api_connection):
+        logger.info("Looking up company mappings")
+        return api_connection.exec_get_url('/api/elvizmapping/companies/')
 
     @staticmethod
     def lookup_company_mapping(api_connection, elviz_company_id):
-        logger.info("Looking up company mappings")
+        logger.info("Looking up company mapping with id " + str(elviz_company_id))
         json_res = api_connection.exec_get_url('/api/elvizmapping/companies/')
         for comp in json_res:
             if comp['elviz_company_id']==elviz_company_id:
@@ -46,6 +40,43 @@ class ElvizLinksApi:
         existing=ElvizLinksApi.lookup_company_mapping(api_connection, elviz_company_id)
         if existing is None:
             json_res = api_connection.exec_post_url('/api/elvizmapping/companies/',payload)
+        else:
+            json_res = api_connection.exec_patch_url('/api/elvizmapping/companies/'
+                                                     + str(existing['pk']) + "/", payload)
+        print(json_res)
+        return True
+
+    @staticmethod
+    def get_user_mappings(api_connection):
+        logger.info("Looking up company mappings")
+        return api_connection.exec_get_url('/api/elvizmapping/companies/')
+
+    @staticmethod
+    def lookup_user_mapping(api_connection, elviz_user_id):
+        logger.info("Looking up company mapping with id " + str(elviz_user_id))
+        json_res = api_connection.exec_get_url('/api/elvizmapping/users/')
+        for comp in json_res:
+            if comp['elviz_user_id'] == elviz_user_id:
+                print("Found user ", comp)
+                return comp
+        return None
+
+    @staticmethod
+    def upsert_user_mapping(api_connection, enegydesk_username, elviz_user_id, elviz_userr_name):
+        logger.info("Register or update company mappingrs")
+        company = UsersApi.get_users(api_connection)
+        if company is None:
+            logger.error("Cannot map a company that is not stored internally with registry " + str(registry_number))
+            return False
+
+        comp_url = api_connection.get_base_url() + "/api/customers/companies/" + str(company['pk']) + "/"
+        payload = {"elviz_company_id": elviz_company_id,
+                   "elviz_company_name": elviz_company_name,
+                   "energydesk_company": comp_url}
+        print("Updating " + str(payload))
+        existing = ElvizLinksApi.lookup_company_mapping(api_connection, elviz_company_id)
+        if existing is None:
+            json_res = api_connection.exec_post_url('/api/elvizmapping/companies/', payload)
         else:
             json_res = api_connection.exec_patch_url('/api/elvizmapping/companies/'
                                                      + str(existing['pk']) + "/", payload)
