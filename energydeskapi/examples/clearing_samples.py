af36b8ad8eb4ae17d6f250a562d68dbb2001a80e
MortenBirkeland <morteb1507@gmail.com>
2022-09-29 11:10:55 +0200

    clearing reports api

--- energydeskapi/examples/clearing_samples.py
+++ energydeskapi/examples/clearing_samples.py
@@ -4,7 +4,6 @@ import requests
 import logging
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.clearing.clearing_api import ClearingApi
-from energydeskapi.types.company_enum_types import CompanyTypeEnum, CompanyRoleEnum
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
@@ -24,10 +23,5 @@ def fetch_clearing_report_types(api_conn):
 if __name__ == '__main__':
 
     api_conn=init_api()
-    #user_profile=CustomersApi.get_user_profile(api_conn)
-    #print(user_profile)
-    #list_users(api_conn)
-    #register_companies(api_conn)
-    #create_company(api_conn)
+    fetch_clearing_reports(api_conn)
     fetch_clearing_report_types(api_conn)
-    #update_company(api_conn)
