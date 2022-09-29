30efdbdc31ee24f009765e0ab02df72a8808a4a9
Steinar <s.r.eriksen@gmail.com>
2022-09-29 09:06:44 +0200

    Added Python SDK files to new repo

--- energydeskapi/geolocation/location_api.py
+++ energydeskapi/geolocation/location_api.py
@@ -21,3 +21,19 @@ class LocationApi:
         if json_res is not None:
             return json_res
         return None
+
+    @staticmethod
+    def get_local_areas(api_connection, location_type_enum):
+        type_pk = location_type_enum if isinstance(location_type_enum, int) else location_type_enum.value
+        """Fetches local area
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        logger.info("Fetching local area geojson")
+        json_res=api_connection.exec_get_url('/api/location/locations/?location_type=' + str(type_pk))
+        if json_res is not None:
+            return json_res
+        return None
+
+
---
+++ energydeskapi/types/location_enum_types.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class LocationTypeEnum(Enum):
+    GENERAL_PRODUCTION_AREA = 1
+    PPA_OFFER_AREA = 2
+    GOs_OFFER_AREA = 3
+
