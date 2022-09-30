daf5bc91e90805ce140c90c24755d1faa226ff6c
MortenBirkeland <morteb1507@gmail.com>
2022-09-30 15:55:19 +0200

    sdk documentation

--- energydeskapi/scheduler/scheduler_api.py
+++ energydeskapi/scheduler/scheduler_api.py
@@ -8,12 +8,17 @@ logger = logging.getLogger(__name__)
 
 
 class SchedulerApi:
-    """ Class for assets
+    """ Class for scheduler
 
     """
 
     @staticmethod
     def get_scheduled_jobs(api_connection):
+        """Fetches scheduled jobs
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
         json_res = api_connection.exec_get_url('/api/schedulemanager/scheduledjobs/')
         if json_res is None:
             return None
@@ -21,6 +26,11 @@ class SchedulerApi:
 
     @staticmethod
     def get_scheduled_jobs_df(api_connection):
+        """Fetches scheduled jobs and shows in a dataframe
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
         json_res = api_connection.exec_get_url('/api/schedulemanager/scheduled-jobs-ext/')
         if json_res is None:
             return None
