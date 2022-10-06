3ef7fb5e5e8f0223e98deb4db7b84714eea1e1e3
Steinar <s.r.eriksen@gmail.com>
2022-10-06 10:09:29 +0200

    Added Python SDK files to new repo

--- energydeskapi/assets/assets_api.py
+++ energydeskapi/assets/assets_api.py
@@ -131,11 +131,9 @@ class AssetsApi:
         :type api_connection: str, required
         """
         json_res = api_connection.exec_get_url('/api/assets/assets/')
-        print(json_res)
         if json_res is None:
             return None
-        df = pd.DataFrame(data=json_res)
-        return df
+        return json_res
 
     @staticmethod
     def get_assets_ext(api_connection):
