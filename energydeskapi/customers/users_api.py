2b308a918c8559fa2425955d184a0435ee76b294
Steinar <s.r.eriksen@gmail.com>
2022-10-12 09:18:03 +0200

    Added Python SDK files to new repo

--- energydeskapi/customers/users_api.py
+++ energydeskapi/customers/users_api.py
@@ -168,6 +168,17 @@ class UsersApi:
             return None
         return json_res
     @staticmethod
+    def get_users_embedded(api_connection, parameters={}):
+        """Fetches user profiles
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        json_res = api_connection.exec_get_url('/api/customers/profiles/embedded/', parameters)
+        if json_res is None:
+            return None
+        return json_res
+
+    @staticmethod
     def get_users_df(api_connection, parameters={}):
         """Fetches user profiles
 
