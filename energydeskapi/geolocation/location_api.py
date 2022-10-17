f1f74609ed8313a9fe7a6f30109fc1c51ff4a12e
Steinar E <s.r.eiksen@gmail.com>
2022-10-17 17:34:14 +0200

    Fixed API error

--- energydeskapi/geolocation/location_api.py
+++ energydeskapi/geolocation/location_api.py
@@ -18,7 +18,7 @@ class LocalArea:
         dict['pk'] = self.pk
         if self.title is not None: dict['title'] = self.title
         if self.geom is not None: dict['geom'] = self.geom
-        if self.location_type is not None: dict['location_type'] = LocationApi.get_location_type_url(parse_enum_type(api_conn,self.location_type))
+        if self.location_type is not None: dict['location_type'] = LocationApi.get_location_type_url(api_conn,parse_enum_type(self.location_type))
         if self.description is not None: dict['description'] = self.description
         if self.is_main_area is not None: dict['is_main_area'] = self.is_main_area
         return dict
