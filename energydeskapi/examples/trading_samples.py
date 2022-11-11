20c8a027163a9917f31e2bd00975d14117644ee0
steinar <s.r.eriksen@gmail.com>
2022-11-11 17:32:35 +0100

    Fixes

--- energydeskapi/examples/trading_samples.py
+++ energydeskapi/examples/trading_samples.py
@@ -1,8 +1,10 @@
 import sys
 import json
-
+from random import randrange
 import requests
 import logging
+import random
+from datetime import datetime
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.lems.lems_api import LemsApi
 from energydeskapi.types.clearing_enum_types import ClearingReportTypeEnum
@@ -14,6 +16,19 @@ logging.basicConfig(level=logging.INFO,
 
 
 
+def gen_orders(api_conn, token, baseprice, ordertype):
+    api_conn.set_token(token, "Token")
+    df = LemsApi.get_traded_products(api_conn)
+    for index, row in df.iterrows():
+        if row['area']=="NO1" or row['area']=="NO2" or row['area']=="NO5":
+            useprice=baseprice
+        else:
+            useprice=baseprice/2
+        qty = 5 + randrange(15)
+        price = useprice + random.uniform(1.5, 5.5)
+        price = round(price, 1)
+        #print("Adding ", row['ticker'], price, qty)
+        LemsApi.add_order(api_conn, row['ticker'], price, qty, ordertype)
 def get_ticker_data(api_conn):
     df=LemsApi.get_traded_products(api_conn)
     #print(df)
@@ -37,6 +52,12 @@ def get_ticker_data(api_conn):
 if __name__ == '__main__':
 
     api_conn=init_api()
+    random.seed(datetime.now())
+    for j in range(5):
+        gen_orders(api_conn, "241a85c905e36c0316d3d58be9cae9d3d5bc7d5a", 65, "BUY")
+        gen_orders(api_conn, "2ba840008a1276d953bb708c0ecd8bf8251355ac", 65, "BUY")
+        gen_orders(api_conn, "03cace913e56d29abc02ec9ebec250913b9b7ee2", 75, "SELL")
+        gen_orders(api_conn, "28bffd50de26b4c9649402ab4c6dc48ca1e391ac", 75, "SELL")
 
-    get_ticker_data(api_conn)
+    gen_orders(api_conn, "23f9dc04336c4a1d62dcd4f37585c4e1d16d58ac", 66, "SELL")
 
