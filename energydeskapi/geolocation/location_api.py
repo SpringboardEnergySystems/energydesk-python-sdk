1a116ef87d74207b963223763b36557e94740b4c
Steinar E <s.r.eiksen@gmail.com>
2022-11-14 00:17:58 +0100

    Fixed API error

--- energydeskapi/geolocation/location_api.py
+++ energydeskapi/geolocation/location_api.py
@@ -103,7 +103,7 @@ class LocationApi:
             else:
                 success, json_res, status_code, error_msg = api_connection.exec_patch_url('/api/locations/localareas/' + str(loc.pk) + "/", loc.get_dict(api_connection))
             
-            #print(success, json_res, status_code, error_msg)
+            print(success, status_code, error_msg)
             return json_res
 
     @staticmethod
