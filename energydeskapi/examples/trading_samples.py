d0232b02341fae612936985a63709c51a0c75c81
Steinar <s.r.eriksen@gmail.com>
2022-11-24 08:38:38 +0100

    Formatting samples

--- energydeskapi/examples/trading_samples.py
+++ energydeskapi/examples/trading_samples.py
@@ -1,13 +1,6 @@
-import sys
-import json
-from random import randrange
-import requests
 import logging
-import random
-from datetime import datetime
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.lems.lems_api import LemsApi
-from energydeskapi.types.clearing_enum_types import ClearingReportTypeEnum
 import pandas as pd
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
@@ -17,11 +10,13 @@ logging.basicConfig(level=logging.INFO,
 
 # Returns an anonymous view of orders in local market
 def get_own_orders(api_conn, ticker):
-    df=LemsApi.query_own_orders(api_conn, ticker)
+    df = LemsApi.query_own_orders(api_conn, ticker)
     print(df)
     return df
 
 # Returns an anonymous view of orders in local market
+
+
 def get_live_orderbook(api_conn):
     df = LemsApi.query_active_orders(api_conn)
     print(df)
@@ -34,61 +29,70 @@ def get_available_products(api_conn):
     print(df)
     return df
 
+
 def remove_all_active_orders(api_conn):
     df_products = get_available_products(api_conn)
     for index, row in df_products.iterrows():
-        df_orders=get_own_orders(api_conn, row['ticker'])
+        df_orders = get_own_orders(api_conn, row['ticker'])
         print("Own orders (incl filled) on ", row['ticker'])
         print(df_orders)
         for index2, row2 in df_orders.iterrows():
-            if row2['order_status']!='ACTIVE':
+            if row2['order_status'] != 'ACTIVE':
                 continue
             LemsApi.remove_order(api_conn, row['ticker'], row2['order_id'])
 
 # Example of adding a buy order to match orders in the live orderbook
-def add_buy_order_on_nearest_products(api_conn,  price,currency, quantity_mw):
-    df_products=get_available_products(api_conn)
-    sample_ticker = df_products["ticker"].values[0]  #For simplicity and test, pick the first product
-    success, json_res, status_code, error_msg = LemsApi.add_order(api_conn,sample_ticker,price, currency, quantity_mw, "BUY", "FOK")
+
+
+def add_buy_order_on_nearest_products(api_conn,  price, currency, quantity_mw):
+    df_products = get_available_products(api_conn)
+    # For simplicity and test, pick the first product
+    sample_ticker = df_products["ticker"].values[0]
+    success, json_res, status_code, error_msg = LemsApi.add_order(
+        api_conn, sample_ticker, price, currency, quantity_mw, "BUY", "FOK")
     if success:
         print(json_res)
     else:
         print(error_msg)
     # Should see filled orders after previous operatgion
-    df=get_own_orders(api_conn, sample_ticker)
+    df = get_own_orders(api_conn, sample_ticker)
     print(df)
 
 
 # Example of retrieving own orders for set of products
 def get_own_orders_per_product(api_conn):
-    df_all_orders=pd.DataFrame()
+    df_all_orders = pd.DataFrame()
     # PS If looping through *all* products, this is the same as querying (once) without specifying the ticker code of the product
-    df_products=get_available_products(api_conn)
+    df_products = get_available_products(api_conn)
     for index, row in df_products.iterrows():
         df_orders = LemsApi.query_own_orders(api_conn, row['ticker'])
-        if len(df_orders)>0:
+        if len(df_orders) > 0:
             print("Found own orders on ", row['ticker'])
-            df_all_orders=df_all_orders.append(df_orders)
+            df_all_orders = df_all_orders.append(df_orders)
     print(df_all_orders)
     return df_all_orders
 
 # Example of retrieving own orders regardless of product
+
+
 def get_own_orders_total(api_conn):
     df_all_orders = LemsApi.query_own_orders(api_conn)
     print(df_all_orders)
     return df_all_orders
 
 # Example of retrieving own orders regardless of product
+
+
 def get_own_trades_total(api_conn):
     df_all_trades = LemsApi.query_own_trades(api_conn)
     print(df_all_trades)
     return df_all_trades
 
+
 if __name__ == '__main__':
-    api_conn=init_api()
-    #add_buy_order_on_nearest_products(api_conn, 1900, "NOK", quantity_mw=3)  # 0.5 MW in sample
-    #remove_all_active_orders(api_conn)
+    api_conn = init_api()
+    # add_buy_order_on_nearest_products(api_conn, 1900, "NOK", quantity_mw=3)  # 0.5 MW in sample
+    # remove_all_active_orders(api_conn)
     get_own_orders_total(api_conn)
     get_own_trades_total(api_conn)
-    #get_live_orderbook(api_conn)
-
+    # get_live_orderbook(api_conn)
--- energydeskapi/lems/lems_api.py
+++ energydeskapi/lems/lems_api.py
@@ -1,5 +1,3 @@
-import requests
-import json
 import logging
 import pandas as pd
 import requests
@@ -7,25 +5,22 @@ import json
 import logging
 import pandas as pd
 from datetime import datetime, timedelta
-from energydeskapi.sdk.common_utils import parse_enum_type
-from energydeskapi.sdk.money_utils import gen_json_money
-from energydeskapi.portfolios.tradingbooks_api import TradingBooksApi
 from energydeskapi.marketdata.markets_api import MarketsApi
-from energydeskapi.customers.customers_api import CustomersApi
-from energydeskapi.customers.users_api import UsersApi
-from moneyed.l10n import format_money
 from energydeskapi.sdk.datetime_utils import convert_datime_to_utcstr
 logger = logging.getLogger(__name__)
+
 
 def check_fix_date2str(dt):
     if isinstance(dt, str):
         return dt
     return convert_datime_to_utcstr(dt)
 
+
 class LocalProduct:
     """ Class for local product
 
     """
+
     def __init__(self,
                  local_market=None,
                  local_area=None,
@@ -43,42 +38,49 @@ class LocalProduct:
                  trader=None,
                  marketplace_product=None
                  ):
-        self.pk=0
-        self.local_market=local_market
-        self.local_area=local_area
-        self.ticker=ticker
-        self.description=description
+        self.pk = 0
+        self.local_market = local_market
+        self.local_area = local_area
+        self.ticker = ticker
+        self.description = description
         self.currency = currency
-        self.traded_from=traded_from
-        self.traded_until=traded_until
-        self.commodity_type=commodity_type
-        self.instrument_type=instrument_type
-        self.contract_status=contract_status
-        self.buy_or_sell=buy_or_sell
-        self.counterpart=counterpart
-        self.market=market
-        self.trader=trader
-        self.marketplace_product=marketplace_product
+        self.traded_from = traded_from
+        self.traded_until = traded_until
+        self.commodity_type = commodity_type
+        self.instrument_type = instrument_type
+        self.contract_status = contract_status
+        self.buy_or_sell = buy_or_sell
+        self.counterpart = counterpart
+        self.market = market
+        self.trader = trader
+        self.marketplace_product = marketplace_product
         self.commodity_delivery_from = None
         self.commodity_delivery_until = None
-        self.product_code=None
-        self.otc_multi_delivery_periods=[]
-        self.tags=[]
-        self.area="SYS"
+        self.product_code = None
+        self.otc_multi_delivery_periods = []
+        self.tags = []
+        self.area = "SYS"
         self.base_peak = "BASE"
         self.spread = False
         self.otc = False
-
 
     def get_dict(self, api_conn):
         dict = {}
         dict['pk'] = self.pk
         prod = {}
-        if self.instrument_type is not None: prod['instrument_type'] = MarketsApi.get_instrument_type_url(api_conn,self.instrument_type)
-        if self.commodity_type is not None: prod['commodity_type'] = MarketsApi.get_commodity_type_url(api_conn, self.commodity_type)
-        if self.commodity_delivery_from is not None:prod['delivery_from'] = check_fix_date2str(self.commodity_delivery_from)
-        if self.commodity_delivery_until is not None: prod['delivery_until'] = check_fix_date2str(self.commodity_delivery_until)
-        prod['area']=self.area
+        if self.instrument_type is not None:
+            prod['instrument_type'] = MarketsApi.get_instrument_type_url(
+                api_conn, self.instrument_type)
+        if self.commodity_type is not None:
+            prod['commodity_type'] = MarketsApi.get_commodity_type_url(
+                api_conn, self.commodity_type)
+        if self.commodity_delivery_from is not None:
+            prod['delivery_from'] = check_fix_date2str(
+                self.commodity_delivery_from)
+        if self.commodity_delivery_until is not None:
+            prod['delivery_until'] = check_fix_date2str(
+                self.commodity_delivery_until)
+        prod['area'] = self.area
         prod['base_peak'] = self.base_peak
         prod['spread'] = self.spread
         prod['otc'] = self.otc
@@ -87,13 +89,13 @@ class LocalProduct:
             prod['product_code'] = self.ticker
         else:
             prod['otc'] = True
-        dict['commodity_definition']=prod
-        dict['ticker'] =self.ticker
-        dict['local_market'] =self.local_market
-        dict['local_area'] =self.local_area
-        dict['currency'] =self.currency
-        dict['traded_from'] =check_fix_date2str(self.traded_from)
-        dict['traded_until'] =check_fix_date2str(self.traded_until)
+        dict['commodity_definition'] = prod
+        dict['ticker'] = self.ticker
+        dict['local_market'] = self.local_market
+        dict['local_area'] = self.local_area
+        dict['currency'] = self.currency
+        dict['traded_from'] = check_fix_date2str(self.traded_from)
+        dict['traded_until'] = check_fix_date2str(self.traded_until)
         return dict
 
 
@@ -109,12 +111,13 @@ class LemsApi:
         :type api_connection: str, required
         """
         logger.info("Registering local market")
-        payload={
-            "description":description,
-            "operator":operator_url,
+        payload = {
+            "description": description,
+            "operator": operator_url,
             "local_areas": areas
         }
-        success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/lems/localmarkets/', payload)
+        success, json_res, status_code, error_msg = api_connection.exec_post_url(
+            '/api/lems/localmarkets/', payload)
         if json_res is None:
             return None
         return json_res
@@ -130,17 +133,19 @@ class LemsApi:
         """
         return api_connection.get_base_url() + '/api/lems/localmarkets/' + str(
             key) + "/"
+
     @staticmethod
-    def upsert_localproduct(api_connection,local_product):
+    def upsert_localproduct(api_connection, local_product):
         """Registers local local product
 
         :param api_connection: class with API token for use with API
         :type api_connection: str, required
         """
         logger.info("Registering local product")
-        payload=local_product.get_dict(api_connection)
+        payload = local_product.get_dict(api_connection)
         print(payload)
-        success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/lems/localproducts/', payload)
+        success, json_res, status_code, error_msg = api_connection.exec_post_url(
+            '/api/lems/localproducts/', payload)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
@@ -154,9 +159,10 @@ class LemsApi:
         :type api_connection: str, required
         """
         logger.info("Fetching counterparts list")
-        if area==None:
-            area="NO1"
-        json_res = api_connection.exec_get_url('/api/lems/tickerdata?area=' + area)
+        if area == None:
+            area = "NO1"
+        json_res = api_connection.exec_get_url(
+            '/api/lems/tickerdata?area=' + area)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
@@ -170,37 +176,40 @@ class LemsApi:
         :type api_connection: str, required
         """
         logger.info("Fetching counterparts list")
-        json_res = api_connection.exec_get_url('/api/lems/localproducts/compact/', parameters)
+        json_res = api_connection.exec_get_url(
+            '/api/lems/localproducts/compact/', parameters)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
         return df
 
     @staticmethod
-    def add_order(api_connection, ticker, price, currency, quantity, buy_or_sell,order_type="NORMAL", expiry=None):
+    def add_order(api_connection, ticker, price, currency, quantity, buy_or_sell, order_type="NORMAL", expiry=None):
         """Fetches all counterparts and displays in a dataframe
 
         :param api_connection: class with API token for use with API
         :type api_connection: str, required
         """
-        exp = (datetime.today() + timedelta(days=2)).replace(hour=0, minute=0, second=0, microsecond=0)
-        expiry_str= check_fix_date2str(exp) if expiry is None else check_fix_date2str(expiry)
-        payload={
-            #"order_id":"",# Will be set by server
-            #"order_timestamp":"", # Will be set by server
-            "ticker":ticker,
-            "price":price,
+        exp = (datetime.today() + timedelta(days=2)
+               ).replace(hour=0, minute=0, second=0, microsecond=0)
+        expiry_str = check_fix_date2str(
+            exp) if expiry is None else check_fix_date2str(expiry)
+        payload = {
+            # "order_id":"",# Will be set by server
+            # "order_timestamp":"", # Will be set by server
+            "ticker": ticker,
+            "price": price,
             "currency": currency,
             "quantity": quantity,
             "expiry": expiry_str,
-            "buy_or_sell":buy_or_sell,
-            "order_type":order_type
+            "buy_or_sell": buy_or_sell,
+            "order_type": order_type
         }
-        success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/lems/addorder/', payload)
+        success, json_res, status_code, error_msg = api_connection.exec_post_url(
+            '/api/lems/addorder/', payload)
         if not success:
             logger.error(error_msg)
         return success, json_res, status_code, error_msg
-
 
     @staticmethod
     def remove_order(api_connection, ticker, order_id):
@@ -209,12 +218,13 @@ class LemsApi:
         :param api_connection: class with API token for use with API
         :type api_connection: str, required
         """
-        payload={
-            "ticker":ticker,
-            "order_id":order_id,
+        payload = {
+            "ticker": ticker,
+            "order_id": order_id,
         }
 
-        success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/lems/removeorder/', payload)
+        success, json_res, status_code, error_msg = api_connection.exec_post_url(
+            '/api/lems/removeorder/', payload)
 
         return success
 
@@ -228,7 +238,7 @@ class LemsApi:
 
         logger.info("Query orders")
         url = '/api/lems/liveorders/' if ticker is None else '/api/lems/liveorders/?ticker=' + ticker
-        json_res=api_connection.exec_get_url(url)
+        json_res = api_connection.exec_get_url(url)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
@@ -243,13 +253,13 @@ class LemsApi:
         """
 
         logger.info("Query own orders")
-        url='/api/lems/myorders/' if ticker is None else '/api/lems/myorders/?ticker=' + ticker
-        json_res=api_connection.exec_get_url(url)
+        url = '/api/lems/myorders/' if ticker is None else '/api/lems/myorders/?ticker=' + ticker
+        json_res = api_connection.exec_get_url(url)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
         return df
-    
+
     @staticmethod
     def query_own_trades(api_connection, ticker=None):
         """Fetches all counterparts and displays in a dataframe
@@ -260,7 +270,7 @@ class LemsApi:
 
         logger.info("Query own trades")
         url = '/api/lems/mytrades/' if ticker is None else '/api/lems/mytrades/?ticker=' + ticker
-        json_res=api_connection.exec_get_url(url)
+        json_res = api_connection.exec_get_url(url)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
