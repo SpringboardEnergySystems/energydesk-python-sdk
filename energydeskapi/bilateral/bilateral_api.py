7ba72c5162405d8fdd3b823e07c1d9274d29486a
Steinar <s.r.eriksen@gmail.com>
2022-11-24 14:13:04 +0100

    Removing debug info

--- energydeskapi/bilateral/bilateral_api.py
+++ energydeskapi/bilateral/bilateral_api.py
@@ -10,7 +10,7 @@ class BilateralApi:
     """
 
     @staticmethod
-    def calculate_contract_price(api_connection ,periods, price_area, currency_code, curve_model):
+    def calculate_contract_price(api_connection ,periods, price_area, currency_code, curve_model, monthly_profile=[], weekday_profile=[]):
         """Fetches hourly price curve
 
         :param api_connection: class with API token for use with API
@@ -34,12 +34,12 @@ class BilateralApi:
             "contract_date_until": p[2],
             })
         qry_payload = {
-            "forward_curve":{
-                    "price_area": price_area,
-                    "currency_code": currency_code,
-                    "curve_model":curve_model,
-                    },
-            "periods":dict_periods,
+                "price_area": price_area,
+                "currency_code": currency_code,
+                "curve_model":curve_model,
+                "periods":dict_periods,
+                "monthly_profile":monthly_profile,
+                "weekday_profile": weekday_profile
         }
 
         #print(qry_payload)
