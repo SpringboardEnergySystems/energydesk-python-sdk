a4f98a9dbb8be3cd2c19834ccf336287410decf4
Steinar <s.r.eriksen@gmail.com>
2022-11-24 08:35:13 +0100

    Removing debug info

--- energydeskapi/examples/trading_samples.py
+++ energydeskapi/examples/trading_samples.py
@@ -15,22 +15,18 @@ logging.basicConfig(level=logging.INFO,
                               logging.StreamHandler()])
 
 
-
-
-
-
 # Returns an anonymous view of orders in local market
 def get_own_orders(api_conn, ticker):
     df=LemsApi.query_own_orders(api_conn, ticker)
     print(df)
     return df
 
-
 # Returns an anonymous view of orders in local market
 def get_live_orderbook(api_conn):
     df = LemsApi.query_active_orders(api_conn)
     print(df)
     return df
+
 
 # Returns an anonymous view of orders in local market
 def get_available_products(api_conn):
@@ -59,14 +55,40 @@ def add_buy_order_on_nearest_products(api_conn,  price,currency, quantity_mw):
     else:
         print(error_msg)
     # Should see filled orders after previous operatgion
-    get_own_orders(api_conn, sample_ticker)
+    df=get_own_orders(api_conn, sample_ticker)
+    print(df)
 
-    #order_id=""  #If picking one of own active orders (none filled) this can be cancelled by following
-    #df = LemsApi.remove_order(api_conn, sample_ticker, order_id)
 
+# Example of retrieving own orders for set of products
+def get_own_orders_per_product(api_conn):
+    df_all_orders=pd.DataFrame()
+    # PS If looping through *all* products, this is the same as querying (once) without specifying the ticker code of the product
+    df_products=get_available_products(api_conn)
+    for index, row in df_products.iterrows():
+        df_orders = LemsApi.query_own_orders(api_conn, row['ticker'])
+        if len(df_orders)>0:
+            print("Found own orders on ", row['ticker'])
+            df_all_orders=df_all_orders.append(df_orders)
+    print(df_all_orders)
+    return df_all_orders
+
+# Example of retrieving own orders regardless of product
+def get_own_orders_total(api_conn):
+    df_all_orders = LemsApi.query_own_orders(api_conn)
+    print(df_all_orders)
+    return df_all_orders
+
+# Example of retrieving own orders regardless of product
+def get_own_trades_total(api_conn):
+    df_all_trades = LemsApi.query_own_trades(api_conn)
+    print(df_all_trades)
+    return df_all_trades
 
 if __name__ == '__main__':
     api_conn=init_api()
-    add_buy_order_on_nearest_products(api_conn, 1900, "NOK", quantity_mw=3)  # 0.5 MW in sample
+    #add_buy_order_on_nearest_products(api_conn, 1900, "NOK", quantity_mw=3)  # 0.5 MW in sample
     #remove_all_active_orders(api_conn)
+    get_own_orders_total(api_conn)
+    get_own_trades_total(api_conn)
+    #get_live_orderbook(api_conn)
 
