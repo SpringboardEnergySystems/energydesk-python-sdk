005181e85361468be87546d007ade99a1da3fbd1
Steinar <s.r.eriksen@gmail.com>
2022-12-12 13:11:36 +0100

    Removing debug info

--- energydeskapi/portfolios/portfoliotree_api.py
+++ energydeskapi/portfolios/portfoliotree_api.py
@@ -9,10 +9,10 @@ logger = logging.getLogger(__name__)
 sample_portfolio_tree=[
   {
     "portfolio_id": 1,
-    "portfolio_name": "Trading Portfolios",
+    "name": "Trading Portfolios",
     "trading_books": [], # Different PKs than portfolios IDs.
     "parent_id": 0,
-    "children": [2]
+    "children": [2,5]
   },
   {
     "portfolio_id": 2,
@@ -47,13 +47,57 @@ sample_portfolio_tree=[
     "children": []
   }]
 
+
+sample_portfolio_tree_embedded=[
+  {
+    "portfolio_id": 1,
+    "name": "Trading Portfolios",
+    "trading_books": [],  # Different PKs than portfolios IDs.
+    "parent_id": 0,
+    "children": [  {
+                "portfolio_id": 2,
+                "name": "Trading Portfolios Nordic",
+                "trading_books": [4,5],  # Different PKs than portfolios IDs.
+                "production_assets": [12,23],  # PKs of assets with FDM forecasts
+                "parent_id": 1,
+                "children": [
+                  {
+                    "portfolio_id": 3,
+                    "name": "Trading Book of Nick Leeson",
+                    "trading_books": [1],  # Different PKs than portfolios IDs.
+                    "production_assets": [],
+                    "parent_id": 2,
+                    "children": []
+                  },
+                  {
+                    "portfolio_id": 4,
+                    "name": "Trading Book of Warren Buffet",
+                    "trading_books": [2],  # Different PKs than portfolios IDs.
+                    "production_assets": [],
+                    "parent_id": 2,
+                    "children": []
+                  },
+                ]
+              },
+              {
+                "portfolio_id": 5,
+                "name": "Trading Book German Power",
+                "trading_books": [6, 7],  # Different PKs than portfolios IDs.
+                "production_assets": [],
+                "parent_id": 1,
+                "children": []
+              }
+
+    ]
+  }
+]
 class PortfolioTreeApi:
 
   @staticmethod
   def get_portfolio_tree(api_connection, parameters={}, root=None):
-    return sample_portfolio_tree
+    return sample_portfolio_tree_embedded
 
   @staticmethod
   def upsert_portfolio_tree(api_connection, tree):
      #Save to backend
-    return True, sample_portfolio_tree
+    return True, sample_portfolio_tree_embedded
