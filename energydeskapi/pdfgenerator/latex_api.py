8eacd7f5439d2aaae75d68c0dd52f1ef4d17bbce
MortenBirkeland <morteb1507@gmail.com>
2022-12-15 09:38:00 +0100

    latex

--- energydeskapi/pdfgenerator/latex_api.py
+++ energydeskapi/pdfgenerator/latex_api.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 class LatexApi:
     @staticmethod
     def exec_post(server_url, payload):
-        server_url = "http://127.0.0.1:5000" + server_url
+        server_url = "http://192.168.1.62:5000" + server_url
         logger.info("Calling URL " + str(server_url))
         logger.debug("...with payload " + str(payload) )
         return requests.post(server_url, json=payload)
