d0fe2e1c14adf3ef227154d44cda87fa631f132b
MortenBirkeland <morteb1507@gmail.com>
2022-12-20 11:40:29 +0100

    portfolio tree

--- energydeskapi/portfolios/portfoliotree_utils.py
+++ energydeskapi/portfolios/portfoliotree_utils.py
@@ -1,6 +1,134 @@
-from energydeskapi.portfolios.portfoliotree_api import sample_portfolio_tree, sample_portfolio_tree_embedded
 import json
 import copy
+
+
+
+sample_portfolio_tree=[
+  {
+    "portfolio_id": 1,
+    "name": "Trading Portfolios",
+    "trading_books": [], # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [12,23],  # PKs of assets with FDM forecasts
+    "parent_id": 0,
+    "children": [2,5]
+  },
+  {
+    "portfolio_id": 2,
+    "name": "Trading Portfolios Nordic",
+    "trading_books": [],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [12,23],  # PKs of assets with FDM forecasts
+    "parent_id": 1,
+    "children": [3,4]
+  },
+  {
+    "portfolio_id": 3,
+    "name": "Trading Book of Nick Leeson",
+    "trading_books": [1], # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 2,
+    "children": []
+  },
+  {
+    "portfolio_id": 4,
+    "name": "Trading Book of Warren Buffet",
+    "trading_books": [2],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 2,
+    "children": [6]
+  },
+  {
+    "portfolio_id": 5,
+    "name": "Trading Book German Power",
+    "trading_books": [3,4],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 1,
+    "children": []
+  },
+  {
+    "portfolio_id": 6,
+    "name": "Trading Book X",
+    "trading_books": [3, 4],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 4,
+    "children": [7]
+  }
+,
+  {
+    "portfolio_id": 7,
+    "name": "Trading Book X",
+    "trading_books": [3, 4],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 4,
+    "children": [8]
+  }
+,
+  {
+    "portfolio_id": 8,
+    "name": "Trading Book X",
+    "trading_books": [3, 4],  # Different PKs than portfolios IDs.
+    "percentage":1,
+    "assets": [],
+    "parent_id": 4,
+    "children": []
+  }
+]
+
+
+sample_portfolio_tree_embedded=[
+  {
+    "portfolio_id": 1,
+    "name": "Trading Portfolios",
+    "trading_books": [],  # Different PKs than portfolios IDs.
+    "parent_id": 0,
+    "children": [  {
+                "portfolio_id": 2,
+                "name": "Trading Portfolios Nordic",
+                "trading_books": [],  # Different PKs than portfolios IDs.
+                "percentage":1,
+                "assets": [12,23],  # PKs of assets with FDM forecasts
+                "parent_id": 1,
+                "children": [
+                  {
+                    "portfolio_id": 3,
+                    "name": "Trading Book of Nick Leeson",
+                    "trading_books": [1],  # Different PKs than portfolios IDs.
+                    "percentage":1,
+                    "assets": [],
+                    "parent_id": 2,
+                    "children": []
+                  },
+                  {
+                    "portfolio_id": 4,
+                    "name": "Trading Book of Warren Buffet",
+                    "trading_books": [2],  # Different PKs than portfolios IDs.
+                    "percentage":1,
+                    "assets": [],
+                    "parent_id": 2,
+                    "children": []
+                  },
+                ]
+              },
+              {
+                "portfolio_id": 5,
+                "name": "Trading Book German Power",
+                "trading_books": [3, 4],  # Different PKs than portfolios IDs.
+                "percentage":1,
+                "assets": [],
+                "parent_id": 1,
+                "children": []
+              }
+
+    ]
+  }
+]
+
 def create_flat_tree(embedded_tree):
     flat_list = []
 
