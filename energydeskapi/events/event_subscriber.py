f59e2ec4618c7dfa933b68b5885b042be9ec8b85
Steinar E <s.r.eiksen@gmail.com>
2023-01-09 06:52:34 +0100

    Adding delivery types

---
+++ energydeskapi/events/event_subscriber.py
@@ -0,0 +1,26 @@
+
+
+
+class EventSubscriber:
+    def __init__(self, topic, callback_function):
+        self.topic=topic
+        self.callback_function=callback_function
+        self.prestrtopic=topic.replace("#","")  # Just used for later loookup
+
+class EventClient:
+    def __init__(self):
+        self.handler_map={}
+    def register_callback(self, event_subscriber):
+        self.handler_map[event_subscriber.topic]=event_subscriber
+
+    def lookup_subscribers(self, topic):
+        subscr=[]
+        for v in self.handler_map.values():
+            if topic.find(v.prestrtopic)>=0:
+                subscr.append(v)
+        return subscr
+
+    def handle_callback(self, topic, message):
+        subscribers=self.lookup_subscribers(topic)
+        for s in subscribers:
+            s.callback_function(topic, message)
--- energydeskapi/events/mqtt_events_api.py
+++ energydeskapi/events/mqtt_events_api.py
@@ -1,6 +1,7 @@
 
 import json
 import logging
+from energydeskapi.events.event_subscriber import EventClient, EventSubscriber
 import paho.mqtt.client as mqtt
 from energydeskapi.sdk.common_utils import init_api
 logging.basicConfig(level=logging.INFO,
@@ -9,31 +10,45 @@ logging.basicConfig(level=logging.INFO,
                               logging.StreamHandler()])
 logger = logging.getLogger(__name__)
 
-class MqttClient:
-    def __init__(self, mqtt_host, mqtt_port, username=None , password=None):
+
+def on_message_callback(client, userdata, message):
+    try:
+        logger.info("Received MQTT message from client " + str(client))
+        content_str = message.payload.decode("utf-8")
+        content = json.loads(content_str)
+        userdata.handle_callback(message.topic, content)
+    except Exception as e:
+        logger.error("Error occured "  + str(e))
+        logger.error(str(content))
+
+class MqttClient(EventClient):
+    def __init__(self, mqtt_host, mqtt_port, username=None , password=None, client_certificate={}):
+        super().__init__()
         self.mqtt_host=mqtt_host
         self.mqtt_port=mqtt_port
         self.username=username
         self.password=password
+        self.client_certificate=client_certificate
 
-    def connect(self, callback_function, topics, client_name="client",  log_error=True):
+    def connect(self,subscriberlist, client_name="client",  log_error=True):
+        self.client=None
         def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
             logger.info("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
             #client.subscribe( topics)  # Subscribe to the topic “digitest/test1”, receive any messages published on it
             paho_topics=[]
-            for t in topics:
-                paho_topics.append((t,2))  #Format is topic name and quality of service 1,2,3
-            if len(topics)>0:
-                client.subscribe(paho_topics)  #Only subscribe if topics given here. Some clients are *publish only*
+            for es in subscriberlist:
+                self.register_callback(es)
+                paho_topics.append((es.topic,2))  #Format is topic name and quality of service 1,2,3
+            if len(paho_topics)>0:
+                self.client.subscribe(paho_topics)  #Only subscribe if topics given here. Some clients are *publish only*
         try:
             logger.info("Initializing MQTT")
             self.client = mqtt.Client(client_name)  # create new instance
-            if callback_function is not None:
-                self.client.on_message = callback_function  # attach function to callback
+            self.client.on_message = on_message_callback  # attach function to callback
             self.client.on_connect = on_connect
             if self.username is not None:
                 self.client.username_pw_set(self.username, self.password)
-            self.topics=topics
+            self.client.user_data_set(self)
             logger.info("Connecting " + str(self.mqtt_host) + ":"  + str(self.mqtt_port))
             self.client.connect(self.mqtt_host, port=int(self.mqtt_port))  # connect to broker
         except Exception as e:
@@ -51,21 +66,10 @@ class MqttClient:
             logger.warning(f"Failed to send message to topic {topic}" + str(result))
     def start_listener(self):
         self.client.loop_start()  # start the loop
-        # logger.info("Setting up MQTT listener on topic" + str(self.subscriber_topic))
-        # self.client.subscribe(topic)
-        # self.client.loop_forever()
+        #self.client.loop_forever()  # Blocking. Not ideal if you want to have main threead available
 
-def on_message_test_callback(client, userdata, message):
-    try:
-        logger.info("Received MQTT message from client " + str(client))
-        content_str = message.payload.decode("utf-8")
-        content = json.loads(content_str)
-        logger.info(content_str)
-    except Exception as e:
-        logger.error("Error occured "  + str(e))
-        logger.error(str(content))
-
-
+def on_my_callback(topic, data):
+    print("GOT CALLBACK",topic, data)
 
 import time, environ
 
@@ -77,8 +81,9 @@ if __name__ == '__main__':
     mqtt_usr=None if "MQTT_USERNAME" not in env else env.str("MQTT_USERNAME")
     mqtt_pwd = None if "MQTT_PASSWORD" not in env else env.str("MQTT_PASSWORD")
     mqttcli=MqttClient(mqtt_broker,mqtt_port,mqtt_usr,mqtt_pwd)
-    mqttcli.connect(on_message_test_callback, ["/marketdata/nordicpower/#"], "Feed Listener")
+    es=EventSubscriber("/marketdata/nordicpower/#",on_my_callback)
+    mqttcli.connect( [es], "Feed Listener")
     mqttcli.start_listener()
-
+    print("Going into final loop")
     while 1==1:
         time.sleep(1)
