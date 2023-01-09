b89f0fcf346d54eee84a75cebaae97cf427eff18
Steinar <s.r.eriksen@gmail.com>
2023-01-09 12:54:06 +0100

    Removing debug info

--- energydeskapi/types/event_types.py
+++ energydeskapi/types/event_types.py
@@ -4,7 +4,8 @@ from enum import Enum
 
 class KafkaQueues(Enum):
     NORDICPOWER_PRICES = "marketdata.prices.nordicpower"
-
+    PRODUCTVIEWS = "portfolioview.productview"
 
 class MqttTopics(Enum):
     NORDICPOWER_PRICES = "/marketdata/nordicpower/#"
+    PRODUCTVIEWS = "/portfolioview/productview"
