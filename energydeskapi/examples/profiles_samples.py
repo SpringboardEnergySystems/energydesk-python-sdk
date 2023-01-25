ddc0900decd38b86647f17dae7dd3474a883a8f2
Steinar E <s.r.eiksen@gmail.com>
2023-01-25 20:24:53 +0100

    Adding Kafka

--- energydeskapi/examples/profiles_samples.py
+++ energydeskapi/examples/profiles_samples.py
@@ -1,7 +1,8 @@
 import logging
 from energydeskapi.sdk.common_utils import init_api
-from energydeskapi.profiles.profiles_api import ProfilesApi
+from energydeskapi.profiles.profiles_api import ProfilesApi, VolumeProfile
 from energydeskapi.types.market_enum_types import CommodityTypeEnum
+from energydeskapi.sdk.pandas_utils import get_winter_profile, get_workweek, get_weekend
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("energydesk_client.log"),
@@ -11,14 +12,24 @@ logging.basicConfig(level=logging.INFO,
 
 
 def load_profiles(api_conn):
-    json_counterparts = ProfilesApi.get_volume_profiles(api_conn)
-    print(json_counterparts)
-
-
-
+    jsdata = ProfilesApi.get_volume_profiles(api_conn)
+    print(jsdata)
+    #jsdata = ProfilesApi.get_spot_profiles(api_conn)
+    #print(jsdata)
+def create_profile(api_conn):
+    v=VolumeProfile()
+    v.profile={
+        'monthly_profile': get_winter_profile(),
+        'weekday_profile': get_workweek(),
+        'daily_profile': list(range(8, 18))
+    }
+    v.description="winterprofile"
+    jsres = ProfilesApi.upsert_volume_profile(api_conn, v)
+    print(jsres)
 
 
 if __name__ == '__main__':
 
     api_conn=init_api()
+    #create_profile(api_conn)
     load_profiles(api_conn)
--- energydeskapi/profiles/profiles_api.py
+++ energydeskapi/profiles/profiles_api.py
@@ -2,10 +2,44 @@ import logging
 from energydeskapi.sdk.common_utils import check_fix_date2str
 logger = logging.getLogger(__name__)
 
+
+
+class VolumeProfile:
+    """ Class for  profiles
+    """
+
+    def __init__(self,
+                 description=None,
+                 profile=None):
+        self.pk=0
+        self.description=description
+        self.profile=profile
+
+    def get_dict(self, api_conn):
+        dict = {}
+        prod = {}
+        prod['pk'] = self.pk
+        prod['description'] = self.description
+        prod['profile'] = self.profile
+        return prod
 class ProfilesApi:
     """Class for profile management
 
     """
+
+    @staticmethod
+    def get_spot_profiles(api_connection, parameters={}):
+        """Fetches credit ratings for counterparts
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        logger.info("Fetching spot profiles")
+        json_res = api_connection.exec_get_url('/api/profilemanager/spotprofiles/', parameters)
+        if json_res is not None:
+            return json_res
+        return None
+
     @staticmethod
     def get_volume_profiles(api_connection, parameters={}):
         """Fetches credit ratings for counterparts
@@ -14,7 +48,17 @@ class ProfilesApi:
         :type api_connection: str, required
         """
         logger.info("Fetching volume profiles")
-        json_res = api_connection.exec_get_url('/api/markets/query-custom-profiles/', parameters)
+        json_res = api_connection.exec_get_url('/api/profilemanager/volumeprofiles/', parameters)
         if json_res is not None:
             return json_res
         return None
+
+    @staticmethod
+    def upsert_volume_profile(api_connection, volume_profile):
+        """Fetches credit ratings for counterparts
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        success, returned_data, status_code, error_msg = api_connection.exec_post_url('/api/profilemanager/volumeprofiles/',volume_profile.get_dict(api_connection))
+        return success, returned_data, status_code, error_msg
