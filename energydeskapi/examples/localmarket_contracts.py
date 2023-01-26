e916575993dffb26a0045be50c8f5d18daea7c49
Steinar <s.r.eriksen@gmail.com>
2023-01-26 18:58:46 +0100

    Updated requirements.txt

--- energydeskapi/examples/localmarket_contracts.py
+++ energydeskapi/examples/localmarket_contracts.py
@@ -11,9 +11,12 @@ from energydeskapi.geolocation.location_api import LocationApi
 from energydeskapi.types.market_enum_types import CommodityTypeEnum, InstrumentTypeEnum, MarketEnum
 from energydeskapi.types.market_enum_types import CommodityTypeEnum, InstrumentTypeEnum
 from datetime import datetime
+from energydeskapi.profiles.profiles_api import ProfilesApi, VolumeProfile
+from energydeskapi.sdk.profiles_utils import get_baseload_weekdays, get_baseload_dailyhours, get_baseload_months
 import logging
+from energydeskapi.lems.lems_api import LemsApi, CustomProfile
 from energydeskapi.sdk.common_utils import init_api
-from datetime import datetime
+from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.customers.customers_api import CustomersApi
@@ -25,6 +28,7 @@ from energydeskapi.contracts.contracts_api import ContractsApi, Contract
 from energydeskapi.types.contract_enum_types import ContractStatusEnum
 from energydeskapi.types.market_enum_types import CommodityTypeEnum, DeliveryTypeEnum,MarketEnum, InstrumentTypeEnum
 from moneyed import NOK
+from dateutil.relativedelta import relativedelta
 from miscutils.timeconversion import current_timestamp_as_utcstr
 from energydeskapi.sdk.money_utils import FormattedMoney
 from uuid import uuid4
@@ -95,10 +99,55 @@ def get_historical_products(api_conn):
     res=LemsApi.get_all_local_products(api_conn)
     generate_contract(res)
 
+def add_custom_profile_order(api_conn, profile, price, quantity):
+    thismonth = date.today().replace(day=1)
+    expiry = date.today() + relativedelta(days=3)
+    dt_from = thismonth + relativedelta(month=3)
+    dt_until = thismonth + relativedelta(month=12)
+    concrete_profile_inc_period = CustomProfile()
+    concrete_profile_inc_period.volume_profile={'monthly_profile':profile['monthly_profile'],
+                    'weekday_profile':profile['weekday_profile'],
+                    'daily_profile':profile['daily_profile'],
+                   }
+    concrete_profile_inc_period.price_area="NO5"
+    concrete_profile_inc_period.delivery_from=dt_from.strftime("%Y-%m-%d")
+    concrete_profile_inc_period.delivery_until = dt_until.strftime("%Y-%m-%d")
+    success, json_res, status_code, error_msg=LemsApi.upsert_custom_profile(api_conn, concrete_profile_inc_period)
+    if success:
+        print("Got ticker to use in order entry", json_res['ticker'])
+        LemsApi.add_order(api_conn, json_res['ticker'], price, "NOK", quantity, "BUY", "NORMAL", expiry.strftime("%Y-%m-%d"), "ACTIVE")
+
+def create_profile(api_conn):
+    months=get_baseload_months()
+    months['June'] = 0.4
+    months['July']=0.4
+    months['August'] = 0.4
+    weekdays=get_baseload_weekdays()
+    hours=get_baseload_dailyhours()
+    v=VolumeProfile()
+    v.profile={
+        'monthly_profile': months,
+        'weekday_profile': weekdays,
+        'daily_profile': hours
+    }
+    v.description="customprofile"
+    jsres = ProfilesApi.upsert_volume_profile(api_conn, v)
+    print(jsres)
+
+def manage_myown_profiles(api_conn):
+    #create_profile(api_conn)
+    jsdata = ProfilesApi.get_volume_profiles(api_conn)
+    print(jsdata)
+
+    jsdata=ProfilesApi.get_volume_profiles(api_conn,{'description':"customprofile"})
+    if len(jsdata['results'])>0:
+        selected_profile=jsdata['results'][0]['profile']
+        print(selected_profile)
+        add_custom_profile_order(api_conn, selected_profile, 1000, 5)
 
 import sys
 if __name__ == '__main__':
     #pd.set_option('display.max_rows', None)
     api_conn=init_api()
-    get_historical_products(api_conn)
+    manage_myown_profiles(api_conn)
 
