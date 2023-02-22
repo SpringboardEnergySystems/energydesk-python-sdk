74d21c816e36e73bca1b11538b048e2e221dfff0
Steinar <s.r.eriksen@gmail.com>
2023-02-22 11:00:18 +0100

    Updated requirements.txt

--- energydeskapi/sdk/pandas_utils.py
+++ energydeskapi/sdk/pandas_utils.py
@@ -16,7 +16,7 @@ def make_empty_timeseries_df(period_from, period_to, pandas_res, timeznoe=None):
     df=pd.DataFrame()
     ix = pd.date_range(start=make_none_tz(period_from), end=make_none_tz(period_to), freq=pandas_res)
     df_new = df.reindex(ix, fill_value='NaN')
-    df_new=df_new.tz_convert(timeznoe)
+    df_new=df_new.tz_localize(timeznoe)
     return df_new
 
 def apply_calendar_pattern_old(df, months, weekdays, hours = range(24)):
