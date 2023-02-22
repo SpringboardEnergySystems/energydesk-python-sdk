871d37573425bbb85d28f0ecb408b087f954156d
MortenBirkeland <morteb1507@gmail.com>
2023-02-22 10:27:49 +0100

    elviz

--- energydeskapi/examples/elviz_samples.py
+++ energydeskapi/examples/elviz_samples.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO,
 
 
 def load_elviz_trades(api_conn):
-    elviz_trades = ElvizLinksApi.get_latest_elviz_trades(api_conn, 20)
+    elviz_trades = ElvizLinksApi.get_latest_elviz_trades(api_conn, 1)
     contracts = []
     for t in elviz_trades:
         market_product_key = 0
