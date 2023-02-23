c3a25cf47f013af7cb026abbac9d664284a281ab
Steinar <s.r.eriksen@gmail.com>
2023-02-23 11:02:34 +0100

    Updated requirements.txt

--- energydeskapi/examples/trading_samples.py
+++ energydeskapi/examples/trading_samples.py
@@ -108,9 +108,9 @@ def get_own_orders_per_product(api_conn):
 
 
 
-def get_contract_doc(api_conn, ticker, quantity, deal_id):
-    r=BilateralApi.load_profiled_volume(api_conn, ticker, float(quantity))
-    print(r)
+def get_contract_doc(api_conn,  deal_id):
+    #r=BilateralApi.load_profiled_volume(api_conn, ticker, float(quantity))
+    #print(r)
     tex_content = LemsApi.get_contract_doc(api_conn,deal_id)
     print(tex_content)
     f=open("./sample.tex","w")
@@ -137,8 +137,11 @@ def get_contract_doc_lastdeal(api_conn):
         return None
     deal=deals.iloc[len(deals.index)-1]
     print(deal)
-    get_contract_doc(api_conn,deal['ticker'],deal['quantity'], deal['deal_id'])
-
+    tex_content=LemsApi.get_contract_doc(api_conn,deal['deal_id'])
+    print(tex_content)
+    f=open("./sample.tex","w")
+    f.write(tex_content['tex_file'])
+    f.close()
 if __name__ == '__main__':
 
     api_conn = init_api()
