3e9be4331a113d0629eb36c01d8e44977875fc12
Steinar <s.r.eriksen@gmail.com>
2023-03-14 12:36:18 +0100

    Updated requirements.txt

--- energydeskapi/examples/fixed_price_samples.py
+++ energydeskapi/examples/fixed_price_samples.py
@@ -5,8 +5,9 @@ from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.bilateral.fixed_price_api import FixedPriceApi
+from energydeskapi.lems.lems_api import LemsApi
 from energydeskapi.profiles.profiles_api import ProfilesApi, StoredProfile
-
+import sys
 from energydeskapi.sdk.profiles_utils import get_baseload_weekdays, get_baseload_dailyhours, get_flat_months,\
 get_default_profile_months
 
@@ -78,14 +79,29 @@ def calculate_price(api_conn, profile_type, custom_profile_key):
         print("Error occured when calculating price")
         print(error_msg)
     return price_offer_id, price
+
 def enter_order_from_priceoffer(api_conn, priceoffer_id, yearly_kwh):
     # This order entry places a limit order at the price given in the offer, with validity time equal to the price offer (can be cancelled)
     success, json_res, status_code, error_msg =FixedPriceApi.add_order_from_priceoffer_id(api_conn, priceoffer_id, "BUY",  yearly_kwh)
-    print(json_res)
+    if not success:
+        return False, error_msg, None
+    return True, json_res['order_id'], json_res['ticker']
 
 def get_available_periods(api_conn):
     periods=FixedPriceApi.get_avaiable_fixprice_periods(api_conn)
-    print(periods)
+    return periods
+
+def remove_last_entered_order(api_conn, series):
+    print("Removing", series['order_id'], series['ticker'])
+    LemsApi.remove_order(api_conn, series['ticker'],series['order_id'])
+
+def get_current_own_orders(api_conn):
+    df=LemsApi.query_own_orders(api_conn)  #Returned as Pandas dataframe
+    if df is None or len(df.index)==0:
+        return None
+    for index,row in df.iterrows():
+        print(row['order_id'],row['ticker'],row['price'],row['quantity'])
+    return df
 
 if __name__ == '__main__':
 
@@ -94,16 +110,25 @@ if __name__ == '__main__':
     # next call: load_current_offers
     # Use priceoffer_id to input a Buy order with a given KWh/year volume.
 
-    # most_recent=get_available_periods(api_conn)
+    periods=get_available_periods(api_conn)
+    print(periods)
 
     load_stored_profiles(api_conn)
     #delete_stored_profiles(api_conn,12)   # Will only delete objects created by company of user
-    #create_save_profile(api_conn)
+    #create_and_save_profile(api_conn)
 
     priceoffer_id, price=calculate_price(api_conn, "BASELOAD", custom_profile_key=None)
-    #calculate_price(api_conn, "CUSDTOM", custom_profile_key=12)  #Use key of stored profiloe
+    #priceoffer_id, price=calculate_price(api_conn, "CUSDTOM", custom_profile_key=12)  #Use key of stored profile
 
-    print("May enter a volume order for price offer ",priceoffer_id, ", Price=", price )
-    #enter_order_from_priceoffer(api_conn, most_recent['priceoffer_id'], 125000)
-    enter_order_from_priceoffer(api_conn,priceoffer_id,125000)  #Yearly KWh at the current moment
+    #print("May enter a volume order for price offer ",priceoffer_id, ", Price=", price )
+    success, order_id, ticker=enter_order_from_priceoffer(api_conn,priceoffer_id,125000)  #Yearly KWh at the current moment
+    if success:
+        print("Order entered successfully:",order_id, ticker)
+    sys.exit(0)
+    df_orders=get_current_own_orders(api_conn)
+    if df_orders is not None:
+        print(df_orders)  # All recent orders, cancelled or active
+        df_active_orders=df_orders.loc[df_orders['order_status']=="ACTIVE"]
+        if df_active_orders is not None:
+            remove_last_entered_order(api_conn,df_active_orders.iloc[-1])
 
