122ec8481a3eb5fc392014dbf8f1bc2ed3953677
Steinar <s.r.eriksen@gmail.com>
2023-03-16 15:22:07 +0100

    Updated requirements.txt

--- energydeskapi/assets/assetdata_api.py
+++ energydeskapi/assets/assetdata_api.py
@@ -76,7 +76,7 @@ def demo_data(api_conn):
     ts.denomination_type_pk=2#AssetDataApi.get_timeseries_adjustment_denomination_type_url(api_conn, 2)
     tss.adjustments.append(ts)
 
-    print(tss.get_dict())
+    return tss.get_dict()
 
 class AssetDataApi:
     """ Class for asset data
