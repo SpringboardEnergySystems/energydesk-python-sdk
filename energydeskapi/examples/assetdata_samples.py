c3a3f39a96df44a2bfdfcc6039c78a5067f36c8d
Steinar <s.r.eriksen@gmail.com>
2023-03-16 13:59:58 +0100

    Updated requirements.txt

--- energydeskapi/examples/assetdata_samples.py
+++ energydeskapi/examples/assetdata_samples.py
@@ -1,7 +1,8 @@
 import logging
 from energydeskapi.sdk.common_utils import init_api
-from energydeskapi.assets.assetdata_api import AssetDataApi
-
+from energydeskapi.assets.assetdata_api import AssetDataApi, TimeSeriesAdjustments, TimeSeriesAdjustment
+from datetime import datetime, timedelta
+from energydeskapi.types.asset_enum_types import AssetForecastAdjustEnum
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("energydesk_client.log"),
@@ -9,14 +10,43 @@ logging.basicConfig(level=logging.INFO,
 
 
 def query_asset_info(api_conn):
-    df = AssetDataApi.get_assetgroup_forecast_df(api_conn, [1, 2, 3])
-    print(df)
-    df = df.resample('MS').sum()
-    df['period']=df.index
-    print(df)
+    res = AssetDataApi.get_latest_forecast(api_conn,{})
+    print(res)
 
+def demo_data(api_conn):
+    curr=datetime.today().strftime(("%Y-%m-%d"))
+    next = (datetime.today() + timedelta(days=1000)).strftime(("%Y-%m-%d"))
+    tss = TimeSeriesAdjustments()
+    tss.is_active_for_asset=True
+    tss.asset_pk=5
+
+    ts=TimeSeriesAdjustment()
+    ts.description="Base rebate"
+    ts.value=0.95
+    ts.adjustment_type_pk=AssetForecastAdjustEnum.PERCENTAGE.value
+    ts.denomination="%"
+    tss.adjustments.append(ts)
+
+
+    ts=TimeSeriesAdjustment()
+    ts.description = "High tax rebate"
+    ts.value=0.30
+    ts.period_from=curr
+    ts.period_until=next
+    ts.adjustment_type_pk=AssetForecastAdjustEnum.PERCENTAGE.value
+    ts.denomination="%"
+    tss.adjustments.append(ts)
+
+    ts=TimeSeriesAdjustment()
+    ts.description = "Option cust rebate"
+    ts.value=0.70
+    ts.adjustment_type_pk=AssetForecastAdjustEnum.EUROP_OPTION.value
+    ts.denomination="NOK strike"
+    tss.adjustments.append(ts)
+
+    print(tss.get_dict())
 
 if __name__ == '__main__':
 
     api_conn = init_api()
-    query_asset_info(api_conn)
+    demo_data(api_conn)
