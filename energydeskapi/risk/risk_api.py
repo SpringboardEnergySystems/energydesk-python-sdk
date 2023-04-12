0017321c0c881f7e8a2ede1cc55b9321d1a82060
steinar <s.r.eriksen@gmail.com>
2023-04-12 18:49:51 +0200

    Updating parsing of portfolio tree

--- energydeskapi/risk/risk_api.py
+++ energydeskapi/risk/risk_api.py
@@ -92,7 +92,7 @@ class RiskApi:
     def calc_covariance_var_df(api_connection, trading_books=[], days_back=40):
         success, json_res, status_code, error_msg=RiskApi.calc_covariance_var(api_connection, trading_books, days_back)
         if success ==False:
-            return None
+            return None,None,None
         var_bins=json_res['var_bins']
         portfolio_mean = json_res['portfolio_mean']
         portfolio_stdev = json_res['portfolio_stdev']
