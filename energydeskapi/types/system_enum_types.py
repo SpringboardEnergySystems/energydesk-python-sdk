57aa8c4aac90c550d7d046921d9082e75102c379
steinar <s.r.eriksen@gmail.com>
2023-04-12 08:15:31 +0200

    Updating parsing of portfolio tree

---
+++ energydeskapi/types/system_enum_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SystemFeaturesEnum(Enum):
+    ASSET_MANAGEMENT = 1
+    RISK_MANAGEMENT = 2
+    STANDARD_CONTRACTS = 3
+    BILATERAL_FIXPRICE = 4
+    CLEARING_MANAGEMENT = 5
+    GOO_CONTRACTS = 6
+    FX_HEDGING = 7
