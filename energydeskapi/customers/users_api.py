cdbfda1e3ec5f7c9800ae0459aa3605956976965
MortenBirkeland <morteb1507@gmail.com>
2023-04-27 10:35:23 +0200

    user feature access delete

--- energydeskapi/customers/users_api.py
+++ energydeskapi/customers/users_api.py
@@ -403,7 +403,7 @@ class UsersApi:
         success, returned_data, status_code, error_msg = api_connection.exec_delete_url('/api/customers/usergroups/' + str(pk) + '/')
         if success:
             return "User group successfully deleted", status_code
-        return None
+        return returned_data, status_code
 
     @staticmethod
     def add_user_to_user_group(api_connection, group_pk, user_pk):
