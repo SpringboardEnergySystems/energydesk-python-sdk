505da5f61bb34d3aceaedcc39e7fdbde9b7cf57b
steinar <s.r.eriksen@gmail.com>
2023-05-01 09:23:16 +0200

    Updating parsing of portfolio tree

--- energydeskapi/portfolios/portfoliotree_utils.py
+++ energydeskapi/portfolios/portfoliotree_utils.py
@@ -241,6 +241,7 @@ def convert_nodes_from_jstree(api_connection, portfolio_nodes):
         if rec['parent'] != "#":
             pid = str(rec['parent'])
         if rec['type'] == "trading_books":
+            print(rec)
             tbdict=TradingBooksApi.get_tradingbooks(api_connection, {'description':rec['data']['original_text']})
             if len(tbdict['results'])>0:
                 pnode=pmap[pnode.pk]
