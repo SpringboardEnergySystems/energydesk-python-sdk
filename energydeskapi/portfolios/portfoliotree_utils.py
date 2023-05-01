905a9f53833a4a900bad40e065e877805686f1d1
steinar <s.r.eriksen@gmail.com>
2023-05-01 09:07:52 +0200

    Updating parsing of portfolio tree

--- energydeskapi/portfolios/portfoliotree_utils.py
+++ energydeskapi/portfolios/portfoliotree_utils.py
@@ -229,7 +229,8 @@ def convert_nodes_from_jstree(api_connection, portfolio_nodes):
         name=rec['data']['original_text'] if 'original_text' in rec['data'] else rec['text']
         pnode=PortfolioNode()
         pnode.description=name
-        print(rec['data']['id'])
+        print(rec)
+        print(rec['data'])
         pnode.pk=int(rec['data']['id'])
         if "company" in rec['data'] and rec['data']['company'] is not None:
             pnode.manager=rec['data']['company']
