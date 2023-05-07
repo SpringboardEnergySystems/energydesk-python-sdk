1fd6f718db2dddb968651e15482f4e8083ee69ad
steinar <s.r.eriksen@gmail.com>
2023-05-07 16:23:08 +0200

    Updating parsing of portfolio tree

--- energydeskapi/scheduler/scheduler_api.py
+++ energydeskapi/scheduler/scheduler_api.py
@@ -7,17 +7,34 @@ logger = logging.getLogger(__name__)
 @dataclass
 class ScheduledJob:
     pk: int
-    job_definition: str
+    job_definition_pk: int
     crontab: str
     dynamic_parameter: str
     is_active: bool
 
+    def get_dict(self, api_conn):
+        dict = {'pk':self.pk}
+        if self.crontab is not None: dict['crontab'] = self.crontab
+        if self.crontab is not None: dict['crontab'] = self.crontab
+        if self.dynamic_parameter is not None: dict['dynamic_parameter'] = self.dynamic_parameter
+        if self.job_definition_pk is not None: dict['job_definition_pk'] = SchedulerApi.get_job_definition_url(api_conn,self.job_definition_pk)
 
+        return dict
 class SchedulerApi:
     """ Class for scheduler
 
     """
+    @staticmethod
+    def get_job_definition_url(api_connection, job_definition):
+        """Fetches url for company types from enum value
 
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        :param company_type_enum: type of company
+        :type company_type_enum: str, required
+        """
+
+        return api_connection.get_base_url() + '/api/schedulemanager/jobdefinitions/' + str(job_definition) + "/"
     @staticmethod
     def upsert_scheduled_job(api_connection, job):
         """Fetches scheduled jobs
