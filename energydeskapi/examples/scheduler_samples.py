78a5f703ac805a4ee1c0d22b88f84e5eeb64ab27
MortenBirkeland <morteb1507@gmail.com>
2023-05-08 11:14:15 +0200

    scheduler

--- energydeskapi/examples/scheduler_samples.py
+++ energydeskapi/examples/scheduler_samples.py
@@ -1,7 +1,7 @@
 
 import logging
 from energydeskapi.sdk.common_utils import init_api
-from energydeskapi.scheduler.scheduler_api import SchedulerApi
+from energydeskapi.scheduler.scheduler_api import SchedulerApi, ScheduledJob
 import pandas as pd
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
@@ -21,8 +21,20 @@ def schedules(api_conn):
     df = pd.DataFrame(data=jobs)
     print(df)
 
+def create_scheduled_job(api_conn):
+    sj = ScheduledJob()
+    sj.pk = 4
+    sj.job_definition_pk = 4
+    sj.crontab = "* * * * *"
+    sj.dynamic_parameter = "5"
+    sj.is_active = True
+
+    result = SchedulerApi.upsert_scheduled_job(api_conn, sj)
+    print(result)
+
 
 if __name__ == '__main__':
 
     api_conn=init_api()
     schedules(api_conn)
+    create_scheduled_job(api_conn)
--- energydeskapi/scheduler/scheduler_api.py
+++ energydeskapi/scheduler/scheduler_api.py
@@ -4,17 +4,18 @@ from dataclasses import dataclass, asdict, field
 
 logger = logging.getLogger(__name__)
 
-@dataclass
+
 class ScheduledJob:
-    pk: int
-    job_definition_pk: int
-    crontab: str
-    dynamic_parameter: str
-    is_active: bool
+    def __init__(self):
+        self.pk = 0
+        self.job_definition_pk = 0
+        self.crontab = None
+        self.dynamic_parameter = None
+        self.is_active = None
 
     def get_dict(self, api_conn):
         dict = {'pk':self.pk}
-        if self.job_definition_pk is not None: dict['job_definition'] = SchedulerApi.get_job_definition_url(api_conn,self.job_definition_pk)
+        if self.job_definition_pk  != 0: dict['job_definition'] = SchedulerApi.get_job_definition_url(api_conn,self.job_definition_pk)
         if self.crontab is not None: dict['crontab'] = self.crontab
         if self.dynamic_parameter is not None: dict['dynamic_parameter'] = self.dynamic_parameter
         if self.is_active is not None: dict['is_active'] = self.is_active
@@ -124,6 +125,19 @@ class SchedulerApi:
         if json_res is None:
             return None
         return json_res
+
+    @staticmethod
+    def get_job_definition(api_connection, pk):
+        """Fetches scheduled job by pk
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        json_res = api_connection.exec_get_url('/api/schedulemanager/jobdefinitions/' + str(pk) + '/')
+        if json_res is None:
+            return None
+        return json_res
+
     @staticmethod
     def get_scheduled_job_execution(api_connection, job_definition_pk):
         """Fetches scheduled jobs and displays in a dataframe
