8510f05caf8169a18ca0fffa3a27be2792cf3118
steinar <s.r.eriksen@gmail.com>
2023-06-11 01:29:05 +0200

    Added params to certificates query

--- energydeskapi/system/default_asset_types.py
+++ energydeskapi/system/default_asset_types.py
@@ -15,7 +15,7 @@ def register_asset_type(api_conn, description):
     #print(success, returned_data, status_code, error_msg)
 
 def initialize_default_etrm_assettypes(api_conn):
-    register_asset_type(api_conn, "Production")
-    register_asset_type(api_conn, "Consumption")
-    register_asset_type(api_conn, "Cleared Account")
-    register_asset_type(api_conn, "Bilateral Trades")
+    register_asset_type(api_conn, "Fuels")
+    register_asset_type(api_conn, "Electricity")
+    register_asset_type(api_conn, "Heat Sales")
+    register_asset_type(api_conn, "Other")
