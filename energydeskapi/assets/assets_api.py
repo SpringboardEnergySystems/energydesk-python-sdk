6a4c7b4165973cccb1e91352dae951c8c883bca6
steinar <s.r.eriksen@gmail.com>
2023-06-11 18:35:19 +0200

    Added params to certificates query

--- energydeskapi/assets/assets_api.py
+++ energydeskapi/assets/assets_api.py
@@ -192,13 +192,13 @@ class AssetsApi:
         return success, returned_data, status_code, error_msg
 
     @staticmethod
-    def get_asset_types(api_connection):
+    def get_asset_types(api_connection, parameters={}):
         """Fetches the type of all assets
 
         :param api_connection: class with API token for use with API
         :type api_connection: str, required
         """
-        json_res = api_connection.exec_get_url('/api/assets/assettypes/')
+        json_res = api_connection.exec_get_url('/api/assets/assettypes/', parameters)
         if json_res is None:
             return None
         df = pd.DataFrame(data=json_res)
