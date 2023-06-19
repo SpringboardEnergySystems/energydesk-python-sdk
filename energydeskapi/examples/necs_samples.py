17145fcf5fe7e315b6b292d86229f1de2ba06596
steinar <s.r.eriksen@gmail.com>
2023-06-19 07:45:24 +0200

    Added params to certificates query

--- energydeskapi/examples/necs_samples.py
+++ energydeskapi/examples/necs_samples.py
@@ -26,39 +26,7 @@ def fetch_necs_certificates(api_conn):
     df.loc['Grand Total', :] = df.sum(axis=0)
     df = df.iloc[np.arange(-1, len(df) - 1)]
     print(df)
-    return
-    p.loc['total'] = p.iloc[:, :-1].sum()
-    #print(p)
-    return
-    dd = p.sum(axis=0, numeric_only=True)
-    print(dd)
-    return
-    df=pd.concat([p, dd])
-    print(df)
-    p = p.sum(numeric_only=True)
-    #df5 = p.sum(axis=1, numeric_only=True)  #
-    print("TOTSUM", p)
-    return
-    df2=df.pivot_table(index="issued_date",values="volume", columns="production_device_name")
-    df5 = df2.sum()
-    print(df5)
-    df5 = df5.sum()
-    print(df5)
-    return
-    df4 = df2.fillna(value=0)
-    #df2=df2.fillna(method="ffill")
-    df2 = df2.fillna(value=0)
-    print(df2)
-    df2['sum'] = df2.sum(axis=1)
-    print(df2)
-    df3=df2.sum(numeric_only=True)#, ignore_index=True)
-    print(df3)
-    #print(df2.columns)
-    #print(df2[['Bagn (Sør-Aurdal)']])
-    #print(df.sort_values(by="completion_date")[['completion_date', 'transaction_volume']])
-    #df = df.astype({'transaction_volume': 'float', 'volume': 'float'})
-    #df2 = df.groupby(by=['production_device_id'], dropna=False).agg({'completion_date':'max','transaction_volume':sum, 'volume':sum})
-    #print(df2)
+
 
 def fetch_necs_certificate_by_key(api_conn):
     pk = 2
@@ -90,12 +58,9 @@ def fetch_necs_transaction_bundles_by_key(api_conn):
 
 
 def fetch_necs_proddevice_versions(api_conn):
-    pk = 3
     result = NecsApi.get_necs_production_device_versions(api_conn)
     df=pd.DataFrame(data=result)
-
     print(df.columns)
-    print(df2)
     #print(df.sort_values(by="licence_expiry"))
 
 if __name__ == '__main__':
