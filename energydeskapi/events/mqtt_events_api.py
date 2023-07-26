89e9a9ba0ba6ec97224f60532d71b431199b54cc
steinar <s.r.eriksen@gmail.com>
2023-07-26 12:50:01 +0200

    Added params to certificates query

--- energydeskapi/events/mqtt_events_api.py
+++ energydeskapi/events/mqtt_events_api.py
@@ -23,6 +23,11 @@ def on_message_callback(client, userdata, message):
         logger.error("Error occured "  + str(e))
         logger.error(str(content))
 
+def on_disconnect(client, userdata, rc):
+    logging.info("disconnecting reason  "  +str(rc))
+    client.connected_flag=False
+    client.disconnect_flag=True
+
 class MqttClient(EventClient):
     def __init__(self, mqtt_host, mqtt_port, username=None , password=None, certificates={}):
         super().__init__()
@@ -37,15 +42,19 @@ class MqttClient(EventClient):
 
     def connect(self,subscriberlist, client_name="client",  log_error=True):
         self.client=None
+
         def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+            print("INSIDE ON CONNECT")
             logger.info("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
             #client.subscribe( topics)  # Subscribe to the topic “digitest/test1”, receive any messages published on it
+            logger.info("So what?")
             paho_topics=[]
             for es in subscriberlist:
                 self.register_callback(es)
                 paho_topics.append((es.topic,2))  #Format is topic name and quality of service 1,2,3
             if len(paho_topics)>0:
                 self.client.subscribe(paho_topics)  #Only subscribe if topics given here. Some clients are *publish only*
+            #self.start_listener()
         try:
             logger.info("Initializing MQTT")
             if self.mqtt_port == "8080":
@@ -56,11 +65,12 @@ class MqttClient(EventClient):
                 print("Using Mqtt")
             self.client.on_message = on_message_callback  # attach function to callback
             self.client.on_connect = on_connect
+            self.client.on_disconnect=on_disconnect
             if self.username is not None:
                 self.client.username_pw_set(self.username, self.password)
             self.client.user_data_set(self)
             logger.info("Connecting " + str(self.mqtt_host) + ":"  + str(self.mqtt_port))
-            print(self.client_certificate)
+            #print(self.client_certificate)
             if self.client_certificate is not None:
                 if self.mqtt_port == "8080":
                     self.client.tls_set(certfile=self.client_certificate,
@@ -69,7 +79,9 @@ class MqttClient(EventClient):
                     self.client.tls_set(ca_certs=self.ca_certificate, certfile=self.client_certificate,
                                         keyfile=self.client_key, tls_version=ssl.PROTOCOL_TLSv1_2)
                 self.client.tls_insecure_set(True)
-            self.client.connect(self.mqtt_host, port=int(self.mqtt_port))  # connect to broker
+
+            x=self.client.connect(self.mqtt_host, port=int(self.mqtt_port))  # connect to broker
+            self.client.loop_forever()
         except Exception as e:
             logger.error(str(e))
             return False
@@ -99,12 +111,13 @@ class MqttClient(EventClient):
             logger.info(f"Send `{msg}` to topic `{topic}`")
         else:
             logger.warning(f"Failed to send message to topic {topic}" + str(result))
+
+    def manual_loop(self):
+        print(self.client.loop())
     def start_listener(self):
         print("Looping")
         result=self.client.loop_start()  # start the loop
-        print(result)
-        print("Started")
-        #self.client.loop_forever()  # Blocking. Not ideal if you want to have main threead available
+        #print(result)
 
 def on_my_callback(topic, data):
     print("GOT CALLBACK",topic, data)
--- energydeskapi/examples/market_simulator.py
+++ energydeskapi/examples/market_simulator.py
@@ -15,6 +15,7 @@ from energydeskapi.marketdata.markets_api import MarketsApi
 from energydeskapi.marketdata.spotprices_api import SpotPricesApi
 from energydeskapi.marketdata.products_api import ProductsApi
 from energydeskapi.types.market_enum_types import MarketEnum, CommodityTypeEnum, InstrumentTypeEnum
+import pendulum
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("energydesk_client.log"),
@@ -51,10 +52,15 @@ def generate_send_marketdata(mqttcli, df,  ticker_idx, ticker):
     mqttcli.publish("/marketdata/nordicpower/prices/nasdaqweb", json.dumps(rec))
 
 def get_current_snapshot(api_conn):
-    df = DerivativesApi.fetch_daily_prices(api_conn, "Nasdaq OMX", "Nordic Power", "ALL")
+    yesterday = pendulum.yesterday('Europe/Oslo')
+    today = pendulum.today('Europe/Oslo')
+    params={"price_date__gte": str(yesterday),"price_date__lt": str(today), 'page_size':1000}
+    #params={'page_size':1000, 'area_filter__in':['SYS',"NO1"]}
+    jd=DerivativesApi.get_prices_flatlist(api_conn, params)
+    #df=DerivativesApi.fetch_prices_in_period(api_conn,market_place= "Nasdaq OMX", market_name="Nordic Power", ticker=None, period_from="2022-12-15", period_until="2023-01-15")
+    df=pd.DataFrame(data=eval(jd['results']))
     print(df)
     return df
-
 
 def pick_test_product():
     c = randrange(4)
@@ -73,45 +79,46 @@ def simulate_price_changes(api_conn, mqttcli):
     df=get_current_snapshot(api_conn)
     df['bid'] = df['last']
     df['ask'] = df['last']
+    counter=1
     while True:
-        ticker_idx = randrange(len(df.index))
-
-        tickrow=df.loc[df['ticker'] == pick_test_product()]
-        ticker_idx=tickrow.index[0]
-
-        ticker = df.loc[ticker_idx, 'ticker']
-
-
-        v = random.uniform(-0.5, 0.5)
-        type_change = randrange(4)
-        if type_change==0:
-            colname="high"
-        elif type_change == 1:
-            colname = "low"
-        elif type_change == 2:
-            colname = "last"
-        else:
-            colname="close"
-        val = df.loc[ticker_idx, colname]
-        val+=v
-        dec_val = float("{:.2f}".format(val))
-        df.loc[ticker_idx, colname]=dec_val
-        print("Sending change for", ticker, colname, dec_val)
-        generate_send_marketdata(mqttcli, df,ticker_idx,ticker)
-        time.sleep(0.1)
+        counter=counter+1
+        if counter>100:
+            ticker_idx = randrange(len(df.index))
+            tickrow=df.loc[df['ticker'] == pick_test_product()]
+            ticker_idx=tickrow.index[0]
+            ticker = df.loc[ticker_idx, 'ticker']
+            v = random.uniform(-0.5, 0.5)
+            type_change = randrange(4)
+            if type_change==0:
+                colname="high"
+            elif type_change == 1:
+                colname = "low"
+            elif type_change == 2:
+                colname = "last"
+            else:
+                colname="close"
+            val = df.loc[ticker_idx, colname]
+            val+=v
+            dec_val = float("{:.2f}".format(val))
+            df.loc[ticker_idx, colname]=dec_val
+            print("Sending change for", ticker, colname, dec_val)
+            generate_send_marketdata(mqttcli, df,ticker_idx,ticker)
+            counter=1
+        time.sleep(.1)
+        #mqttcli.manual_loop()
 
 if __name__ == '__main__':
     random.seed(datetime.now())
     api_conn=init_api()
     env = environ.Env()
     mqtt_broker = env.str('MQTT_HOST')
-    mqtt_port= env.str('MQTT_WEBSOCKET_PORT')
+    mqtt_port= env.str('MQTT_PORT')
     mqtt_usr=None if "MQTT_USERNAME" not in env else env.str("MQTT_USERNAME")
     mqtt_pwd = None if "MQTT_PASSWORD" not in env else env.str("MQTT_PASSWORD")
     client_cert = None if "MQTT_CLIENT_CERT" not in env else env.str('MQTT_CLIENT_CERT')
     client_key = None if "MQTT_CLIENT_KEY" not in env else env.str('MQTT_CLIENT_KEY')
     certificates= {'client_certificate': client_cert, 'client_key': client_key}
-    mqttcli=MqttClient(mqtt_broker,mqtt_port,mqtt_usr,mqtt_pwd, certificates)
-    mqttcli.connect(None, [], "Feed Sender")
-
+    mqttcli=MqttClient(mqtt_broker,mqtt_port,mqtt_usr,mqtt_pwd)
+    mqttcli.connect([], "Feed Sender")
+    #mqttcli.start_listener()
     simulate_price_changes(api_conn, mqttcli)
