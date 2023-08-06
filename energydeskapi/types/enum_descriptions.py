d14500c6d6cf3ee93bfef70cb8bc26e5d6cc6310
steinar <s.r.eriksen@gmail.com>
2023-08-06 11:34:35 +0200

    Added params to certificates query

--- energydeskapi/types/enum_descriptions.py
+++ energydeskapi/types/enum_descriptions.py
@@ -1,4 +1,15 @@
-from energydeskapi.types.market_enum_types import MarketEnum
+from energydeskapi.types.market_enum_types import MarketEnum, ProfileTypeEnum
+
+
+def lookup_profile_description(atype):
+    if atype==ProfileTypeEnum.BASELOAD:
+        return "Baseload"
+    elif atype==ProfileTypeEnum.PROFILE:
+        return "Relative Profile"
+    elif atype==ProfileTypeEnum.ABSOLUTEPROFILE:
+        return "Absolute Profile"
+    else:
+        return "Unknown Profile"
 
 def lookup_market_description(market_enum):
     if market_enum==MarketEnum.NORDIC_POWER:
