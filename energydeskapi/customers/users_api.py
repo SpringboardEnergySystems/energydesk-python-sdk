3a5cfba9adab17c0a4b563a10d7da3a640ff07ee
steinar <s.r.eriksen@gmail.com>
2023-08-07 11:40:15 +0200

    Added params to certificates query

--- energydeskapi/customers/users_api.py
+++ energydeskapi/customers/users_api.py
@@ -16,6 +16,7 @@ class User:
         self.is_super_user=False
         self.company=None
         self.company_registry_number=None
+        self.password=None
 
     def get_dict(self):
         dict = {}
@@ -26,6 +27,7 @@ class User:
         if self.first_name is not None: dict['first_name'] = self.first_name
         if self.last_name is not None: dict['last_name'] = self.last_name
         if self.user_role is not None: dict['user_role'] = self.user_role
+        if self.password is not None and self.password!="": dict['password'] = self.password
         if self.company is not None: dict['company'] = self.company
         if self.company_registry_number is not None: dict['company_registry_number'] = self.company_registry_number
         if self.is_super_user is not None: dict['is_super_user'] = self.is_super_user
