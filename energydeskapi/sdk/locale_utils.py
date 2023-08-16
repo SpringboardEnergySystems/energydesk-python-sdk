af7e18fa2ffa754ce1ad0cbfd5f7713597cf9bd3
steinar <s.r.eriksen@gmail.com>
2023-08-16 09:55:19 +0200

    Added params to certificates query

--- energydeskapi/sdk/locale_utils.py
+++ energydeskapi/sdk/locale_utils.py
@@ -1,7 +1,11 @@
 from energydeskapi.types.common_enum_types import CountryPrefEnum
 #import locale
+import pytz
+from datetime import datetime
+from dateutil import parser
+import pendulum
 from babel.numbers import format_decimal as babel_format_decimal, decimal as babel_decimal
-
+from babel.dates import format_datetime as babel_format_datetime, format_date as babel_format_date
 def get_country_code(country_pref_enum=CountryPrefEnum.NORWAY):
     if country_pref_enum==CountryPrefEnum.NORWAY:
         return "nb_NO.utf-8"
@@ -20,8 +24,24 @@ def format_decimal(dec, country_pref_enum=CountryPrefEnum.NORWAY, decimal_places
     with babel_decimal.localcontext(babel_decimal.Context(rounding=babel_decimal.ROUND_HALF_UP)):
         return babel_format_decimal(dec, format='#,##0.' + dec_pattern + ';-#', locale=get_country_code(country_pref_enum), decimal_quantization=truncate)
 
+
+def format_datetime_from_dt(dt, tzinfo=pytz.timezone('Europe/Oslo'),country_pref_enum=CountryPrefEnum.NORWAY):
+    return babel_format_datetime(dt, tzinfo=tzinfo, locale=get_country_code(country_pref_enum))
+
+def format_datetime_from_iso(dts, tzinfo=pytz.timezone('Europe/Oslo'), country_pref_enum=CountryPrefEnum.NORWAY):
+    dt=parser.isoparse(dts)
+    dt=tzinfo.localize(dt)
+    return babel_format_datetime(dt, tzinfo=tzinfo, locale=get_country_code(country_pref_enum))
+# Usage
+#     df_trades['quantity']=df_trades.apply(format_pandas_decimalcol,colname = "quantity", country_pref_enum=CountryPrefEnum.NORWAY, axis=1)
 def format_pandas_decimalcol(row, colname, country_pref_enum=CountryPrefEnum.NORWAY, decimal_places=2, truncate=True):
     return format_decimal(row[colname],country_pref_enum,decimal_places, truncate)
 
+def format_pandas_datetimecol_from_iso(row, colname, tzinfo=pytz.timezone('Europe/Oslo'), country_pref_enum=CountryPrefEnum.NORWAY):
+    return format_datetime_from_iso(row[colname],tzinfo, country_pref_enum)
+
+import pendulum
 if __name__ == '__main__':
     print(format_decimal(1000000.23533))#, CountryPrefEnum.NORWAY,decimal_places=3, truncate=True))
+    print(format_datetime_from_dt(datetime.now()))
+    print(format_datetime_from_iso("2023-10-01"))
