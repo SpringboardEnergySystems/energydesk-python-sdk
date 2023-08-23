518cc660ec67fb7cfcd99854ae688c0f8bf497aa
steinar <s.r.eriksen@gmail.com>
2023-08-23 11:07:36 +0200

    Added params to certificates query

--- energydeskapi/sdk/datetime_utils.py
+++ energydeskapi/sdk/datetime_utils.py
@@ -116,7 +116,3 @@ def next_weekday(d, weekday):
     if days_ahead <= 0: # Target day already happened this week
         days_ahead += 7
     return d + timedelta(days_ahead)
-
-    #d = datetime.date(2011, 7, 2)
-    #next_monday = next_weekday(d, 0) # 0 = Monday, 1=Tuesday, 2=Wednesday...
-    #print(next_monday)
