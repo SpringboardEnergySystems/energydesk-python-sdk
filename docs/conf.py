1a9a142485187dbf721bb96433cb362f2d7c941e
steinar <s.r.eriksen@gmail.com>
2023-09-04 14:44:18 +0200

    Adding classes for credit rating

--- docs/conf.py
+++ docs/conf.py
@@ -1,61 +1,36 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
+# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
-
 # -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'EnergyDesk SDK'
-copyright = '2022, Springboard Energy Systems'
-author = 'Springboard Energy Systems'
-
-# The full version, including alpha/beta/rc tags
-release = '1.0'
-
+project = 'energydeskapi'
+copyright = '2023, Author'
+author = 'Author'
 
 # -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-master_doc = 'index'
-
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'sphinx_rtd_theme'
-#import sphinx_theme
-#html_theme = 'stanford_theme'
-#html_theme_path = [sphinx_theme.get_html_theme_path('stanford-theme')]
+html_theme = 'alabaster'
+html_static_path = ['_static']
 
+# -- Options for todo extension ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
 
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['./_build/html/_static']
+todo_include_todos = True
--- docs/index.rst
+++ docs/index.rst
@@ -1,16 +1,17 @@
-.. EnergyDesk SDK documentation master file, created by
-   sphinx-quickstart on Tue Aug 23 23:32:08 2022.
+.. energydeskapi documentation master file, created by
+   sphinx-quickstart on Fri Sep  1 13:47:09 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to EnergyDesk SDK's documentation!
-==========================================
+Welcome to energydeskapi's documentation!
+=========================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 4
    :caption: Contents:
 
-   modules
+   energydeskapi
+
 
 Indices and tables
 ==================
