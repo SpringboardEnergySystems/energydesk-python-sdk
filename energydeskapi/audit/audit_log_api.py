f9c3d19ce0f447c833378b52d8ca2a2a67756e89
steinar <steinar.eriksen@hafslundeco.no>
2023-09-20 14:23:07 +0200

    Portfolio View

---
+++ energydeskapi/audit/audit_log_api.py
@@ -0,0 +1,44 @@
+import logging
+import pandas as pd
+logger = logging.getLogger(__name__)
+
+
+class AuditLogApi:
+    """ Class for audit log REST API
+    """
+
+    @staticmethod
+    def get_audit_log(api_connection, parameters={}):
+        """Fetches all assets
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        json_res = api_connection.exec_get_url('/api/audit/auditlog/', parameters)
+        if json_res is None:
+            return None
+        return json_res
+
+    @staticmethod
+    def get_audit_log_embedded(api_connection, parameters={}):
+        """Fetches audit log with embedded structure for user and contracts
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        json_res = api_connection.exec_get_url('/api/audit/auditlog/embedded/', parameters)
+        if json_res is None:
+            return None
+        return json_res
+
+    @staticmethod
+    def get_audit_log_types(api_connection, parameters={}):
+        """Fetches all audit log types
+
+        :param api_connection: class with API token for use with API
+        :type api_connection: str, required
+        """
+        json_res = api_connection.exec_get_url('/api/audit/auditlogtypes/', parameters)
+        if json_res is None:
+            return None
+        return json_res
---
+++ energydeskapi/examples/auditlog_samples.py
@@ -0,0 +1,28 @@
+import json
+import logging
+from energydeskapi.system.default_asset_types import initialize_default_etrm_assettypes
+from energydeskapi.audit.audit_log_api import AuditLogApi
+from energydeskapi.sdk.common_utils import init_api
+from energydeskapi.types.asset_enum_types import AssetCategoryEnum
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    handlers=[logging.FileHandler("energydesk_client.log"),
+                              logging.StreamHandler()])
+
+
+def get_audit_log_types(api_conn):
+    jsondata = AuditLogApi.get_audit_log_types(api_conn)
+    print(json.dumps(jsondata, indent=2))
+
+def get_audit_log(api_conn):
+    jsondata = AuditLogApi.get_audit_log(
+        api_conn, {})
+    print(json.dumps(jsondata, indent=2))
+
+if __name__ == '__main__':
+
+    api_conn = init_api()
+
+    get_audit_log_types(api_conn)
+    get_audit_log(api_conn)
