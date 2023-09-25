94946f847930ad6ec9cdd8cfeb02758c921b08a1
steinar <steinar.eriksen@hafslundeco.no>
2023-09-25 15:08:07 +0200

    Portfolio View

--- energydeskapi/gos/gos_utils.py
+++ energydeskapi/gos/gos_utils.py
@@ -18,8 +18,8 @@ def generate_product_code_fromgocontract(api_conn, contract):
         return "GoO Contract without Certificate info"
     cert_contract=contract.certificates[0]
     print(cert_contract)
-    #asset_json=AssetsApi.get_asset_by_key(api_conn, gofields.asset)
-    return "GoO_"# + asset_json['description'] + "_" + str(gofields.delivery_date)[:10]
+    asset_json=AssetsApi.get_asset_by_key(api_conn, cert_contract.asset)
+    return "GoO_" + asset_json['description'] + "_" + str(cert_contract.delivery_date)[:10]
 
 def generate_product_code(api_conn, contract, gofields):
     asset_json=AssetsApi.get_asset_by_key(api_conn, gofields.asset)
