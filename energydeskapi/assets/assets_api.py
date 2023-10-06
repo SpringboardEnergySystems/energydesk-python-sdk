24541a4b6f5e299b5fb22ebcaceacd52a77fa491
steinar <s.r.eriksen@gmail.com>
2023-10-06 10:29:07 +0200

    Fixes

--- energydeskapi/assets/assets_api.py
+++ energydeskapi/assets/assets_api.py
@@ -170,6 +170,7 @@ class AssetsApi:
         for asset in asset_list:
             payload=asset.get_dict()
             print(payload)
+
             success, json_res, status_code, error_msg=api_connection.exec_post_url('/api/assets/assets/', payload)
             if json_res is None:
                 logger.error("Problems registering asset "  + asset.description)
