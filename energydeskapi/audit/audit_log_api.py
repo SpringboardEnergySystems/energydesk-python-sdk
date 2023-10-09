7274986e8b0d0cd5dc61257e4343631929f751a8
steinar <s.r.eriksen@gmail.com>
2023-10-09 14:46:19 +0200

    Fixes

--- energydeskapi/audit/audit_log_api.py
+++ energydeskapi/audit/audit_log_api.py
@@ -32,13 +32,14 @@ class AuditLogApi:
         return json_res
 
     @staticmethod
-    def get_audit_log_object(api_connection, pk, parameters={}):
+    def get_audit_log_object(api_connection, pk):
         """Fetches audit log with embedded structure for user and contracts
 
         :param api_connection: class with API token for use with API
         :type api_connection: str, required
         """
-        json_res = api_connection.exec_get_url('/api/audit/auditlog/embedded/?' + str(pk), parameters)
+        parameters = {'audit_object_id':pk}
+        json_res = api_connection.exec_get_url('/api/audit/auditlog/embedded/', parameters)
         if json_res is None:
             return None
         return json_res
