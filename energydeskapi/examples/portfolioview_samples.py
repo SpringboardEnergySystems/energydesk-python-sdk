c91cae1476fb0c984a9cdecc03130351b8fe9837
steinar <s.r.eriksen@gmail.com>
2023-10-12 11:02:24 +0200

    Fixes

--- energydeskapi/examples/portfolioview_samples.py
+++ energydeskapi/examples/portfolioview_samples.py
@@ -26,6 +26,8 @@ def get_period_view(api_conn):
         "view_period_until__lt": '2023-05-01',
         "commodity__delivery_from": '2023-01-01',
         "commodity__delivery_until": '2025-01-01',
+        #"trade_date__gte": '2024-01-01',
+        "trade_date__lt": '2024-01-01',
         "resolution":PeriodResolutionEnum.HOURLY.value,
         "groupby":PeriodViewGroupingEnum.AREA.value
     }
