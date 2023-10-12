928e17abc0f05074252da3df82049880c002dae2
steinar <s.r.eriksen@gmail.com>
2023-10-12 08:21:51 +0200

    Fixes

--- energydeskapi/types/market_enum_types.py
+++ energydeskapi/types/market_enum_types.py
@@ -12,6 +12,12 @@ class MarketEnum(Enum):
     CURRENCY_MARKET = 4
     GOs_MARKET = 5
 
+class MarketPlaceTypeEnum(Enum):
+    SPOT_COMMODITIES = 1   # Key for the most commonly used markets
+    FUTURES_COMMODITIES = 2
+    BILATERAL_FORWARD_COMMODITIES = 3
+
+
 class DeliveryTypeEnum(Enum):
     FINANCIAL = 1
     PHYSICAL = 2
@@ -23,6 +29,7 @@ class CommodityTypeEnum(Enum):
     GAS = 4
     GOs = 5
     CURRENCY = 6
+    GRID_CAPACITY = 7
 
 class InstrumentTypeEnum(Enum):
     FUT = 1
@@ -41,3 +48,54 @@ class BlockSizeEnum(Enum):
     QUARTER = 6
     SEASON = 7
     YEAR = 8
+
+
+
+def delivery_type_description(x):
+    return {
+        DeliveryTypeEnum.FINANCIAL: "Financial Delivery",
+        DeliveryTypeEnum.PHYSICAL: "Physical Delivery",
+    }[x]
+
+def commodity_type_description(x):
+    return {
+        CommodityTypeEnum.POWER: "Power",
+        CommodityTypeEnum.EUA: "EUA",
+        CommodityTypeEnum.ELCERT: "Elcertificate",
+        CommodityTypeEnum.GAS: "Gas",
+        CommodityTypeEnum.GOs: "Guarantees of Origin",
+        CommodityTypeEnum.CURRENCY: "Currency",
+        CommodityTypeEnum.GRID_CAPACITY: "Grid Capacity",
+    }[x]
+
+def blocksize_description(x):
+    return {
+        BlockSizeEnum.SPOT: "Spot Product",
+        BlockSizeEnum.DAY: "Day Product",
+        BlockSizeEnum.WEEK: "Week Product",
+        BlockSizeEnum.WEEKEND: "Weekend Product",
+        BlockSizeEnum.MONTH: "Month Product",
+        BlockSizeEnum.QUARTER: "Quarterly Product",
+        BlockSizeEnum.SEASON: "Seasonal Product",
+        BlockSizeEnum.YEAR: "Yearly Product"
+    }[x]
+
+
+def instrument_type_description(x):
+    return {
+        InstrumentTypeEnum.FUT: "Future",
+        InstrumentTypeEnum.FWD: "Forward",
+        InstrumentTypeEnum.SPOT: "Spot",
+        InstrumentTypeEnum.EPAD: "EPAD",
+        InstrumentTypeEnum.EUROPT: "European Option",
+        InstrumentTypeEnum.ASIOPT: "Asian Option",
+    }[x]
+
+
+def marketplace_type_description(x):
+    return {
+        MarketPlaceTypeEnum.SPOT_COMMODITIES: "Spot Commodity Markets",
+        MarketPlaceTypeEnum.FUTURES_COMMODITIES: "Futures Commodity Markets",
+        MarketPlaceTypeEnum.BILATERAL_FORWARD_COMMODITIES: "Bilateral Commodity Markets",
+
+    }[x]
