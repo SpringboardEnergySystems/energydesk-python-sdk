1025366a6012171e12aab6613e1a7bf3c7887879
steinar <s.r.eriksen@gmail.com>
2023-10-17 07:54:41 +0200

    Fixes

--- energydeskapi/examples/optimization_samples.py
+++ energydeskapi/examples/optimization_samples.py
@@ -2,7 +2,7 @@ import logging
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.energydesk.general_api import GeneralApi
 from energydeskapi.optimization.battery_optimizer_api import BatteryOptimizationApi, OptimizerInput
-
+import pendulum
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
                     handlers=[logging.FileHandler("energydesk_client.log"),
@@ -13,6 +13,20 @@ logging.basicConfig(level=logging.INFO,
 
 def optimize_battery(api_conn):
     indata=OptimizerInput()
+    #from_time = serializers.DateTimeField(help_text=("Optimize From Time"))
+    #until_time = serializers.DateTimeField(help_text=("Optimize Until Time"))
+    #customer_profile= serializers.IntegerField(help_text=("Customer Profile TYpe"))
+    #yearly_kwh = serializers.FloatField(help_text=("Yearly Consumption KWh"))
+
+    #battery_capacity_kwh = serializers.FloatField(help_text=("Battery Capacity KWh"))
+    ##battery_charging_power_kw = serializers.FloatField(help_text=("Discharging Power KW"))
+    #battery_dicharging_power_kw = serializers.FloatField(help_text=("Discharging Power KW"))
+    indata.optimize_fromdate=pendulum.today().subtract(months=3)
+    indata.optimize_untildate=pendulum.today().subtract(months=1)
+    indata.capacity_kwh=100
+    indata.charging_power_kw=5
+    indata.dis_charging_power_kw=5
+
     BatteryOptimizationApi.optimize_battery(api_conn, indata)
 
 
--- energydeskapi/optimization/battery_optimizer_api.py
+++ energydeskapi/optimization/battery_optimizer_api.py
@@ -9,24 +9,30 @@ class OptimizerInput:
                  charging_efficiency=0.95, max_soc=0.9, min_soc=0.1):
         self.capacity_kwh=capacity_kwh
         self.charging_power_kw=charging_power_kw
-        self.dis_charging_power_kw=dis_charging_power_kw
+        self.discharging_power_kw=dis_charging_power_kw
         self.charging_efficiency=charging_efficiency
         self.max_soc=max_soc
         self.min_soc = min_soc
         self.optimize_fromdate=optimize_fromdate
         self.optimize_untildate=optimize_untildate
 
+        self.mainmeter_yearly_kwh=2000000
+        self.mainmeter_customer_profile=1
+
     def get_dict(self):
         dict = {}
 
         if self.capacity_kwh is not None: dict['capacity_kwh'] = self.capacity_kwh
         if self.charging_power_kw is not None: dict['charging_power_kw'] = self.charging_power_kw
-        if self.dis_charging_power_kw is not None: dict['dis_charging_power_kw'] = self.dis_charging_power_kw
+        if self.discharging_power_kw is not None: dict['discharging_power_kw'] = self.discharging_power_kw
         if self.charging_efficiency is not None: dict['charging_efficiency'] = self.charging_efficiency
         if self.max_soc is not None: dict['max_soc'] = self.max_soc
         if self.min_soc is not None: dict['min_soc'] = self.min_soc
         if self.optimize_fromdate is not None: dict['optimize_fromdate'] = str(self.optimize_fromdate)
         if self.optimize_untildate is not None: dict['optimize_untildate'] = str(self.optimize_untildate)
+
+        if self.mainmeter_yearly_kwh is not None: dict['mainmeter_yearly_kwh'] = str(self.mainmeter_yearly_kwh)
+        if self.mainmeter_customer_profile is not None: dict['mainmeter_customer_profile'] = str(self.mainmeter_customer_profile)
 
         return dict
 
