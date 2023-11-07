de22767a2f840c4332c054cdc201d7f8c6851ff0
steinar <steinar.eriksen@hafslundeco.no>
2023-11-07 09:54:21 +0100

    Portfolio View

--- energydeskapi/sdk/datetime_utils.py
+++ energydeskapi/sdk/datetime_utils.py
@@ -61,16 +61,29 @@ def convert_datime_to_locstr(dt, loczone="Europe/Oslo"):
     )
     return timestamp_string
 
+def localized_datetimestr_from_datetime(datetime_val, loczone="Europe/Oslo"):
+    """ Converts datetime from UTC
+    """
+    if type(datetime_val) == str:
+        pendulum.parse(datetime_val, loczone).to_datetime_string()
+    else:
+        pendulum.from_timestamp(datetime_val, loczone).to_datetime_string()
+
+    return d_loc
+
+# Make sure we are in normal time when getting date part
+# I.e. 2024-12-31T23:00:00+00:00  becomes 2025-01-01 in Europe/Oslo
+def localized_datestr_from_datetime(datetime_val, loczone="Europe/Oslo"):
+    tmp=localized_datetimestr_from_datetime(datetime_val, loczone)
+    return tmp[:10]
+
+
+
 
 # Data retrieved from server are in UTC time ("GMT without daylight savings time")
 # This function converts to local time
 def convert_datetime_from_utc(utc_str, loczone="Europe/Oslo"):
     """ Converts datetime from UTC
-
-    :param utc_str: string of UTC
-    :type utc_str: str, required
-    :param loczone: specified location
-    :type loczone: str, required
     """
     timezone = pytz.timezone(loczone)
     utc_dt=parser.isoparse(str(utc_str))
