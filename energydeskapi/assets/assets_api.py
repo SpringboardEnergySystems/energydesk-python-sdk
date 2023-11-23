7d13d40842a818397691a059520ead4c6a980b6d
steinar <s.r.eriksen@gmail.com>
2023-11-23 16:32:05 +0100

    Fixes

--- energydeskapi/assets/assets_api.py
+++ energydeskapi/assets/assets_api.py
@@ -328,7 +328,7 @@ class AssetsApi:
         json_res = api_connection.exec_get_url('/api/assets/assets/embedded', parameters)
         return json_res
     @staticmethod
-    def get_assets_compact(api_connection, parameters={}):   # Method allowed for anonymous users, only showing simple info
+    def get_assets_compact(api_connection, parameters={}):
         """Fetches all assets with extended information
 
         :param api_connection: class with API token for use with API
