ba3f6d2e0c24ee148197523db81da243907632d1
steinar <s.r.eriksen@gmail.com>
2024-01-17 13:31:01 +0100

    Fixes

--- energydeskapi/marketdata/products_api.py
+++ energydeskapi/marketdata/products_api.py
@@ -127,6 +127,7 @@ class ProductsApi:
     def generate_market_product_from_ticker(api_connection, market, market_ticker):
         success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/markets/gen-marketproduct/',
                                                 {'market_ticker':market_ticker, 'market':market})
+        print(status_code, json_res)
         return success, json_res, status_code, error_msg
 
     @staticmethod
