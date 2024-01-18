b7a4499f143f4457f907d1e2c582ec6ff7013c5b
aferrandi <ferrandi.andrea@gmail.com>
2024-01-18 13:31:57 +0100

    EDSK-339: added the possibility to use a password to connect to redis

--- energydeskapi/sdk/redis/redis_connection.py
+++ energydeskapi/sdk/redis/redis_connection.py
@@ -10,9 +10,10 @@ def connect_to_redis():
         redis_timeput = "1" if not "REDIS_TIMEOUT_SECONDS" in env else env.str("REDIS_TIMEOUT_SECONDS")
         red_port = "6379" if not "REDIS_PORT" in env else env.str("REDIS_PORT")
         red_db = "1" if not "REDIS_DB" in env else env.str("REDIS_DB")
+        red_password = None if not "REDIS_PASSWORD" in env else env.str("REDIS_PASSWORD")
         logger.info("Connecting to redis " + str(red_host) + ":" + str(red_port) + " db" + str(red_db))
         redis_connection = redis.StrictRedis(host=red_host, port=int(red_port), db=int(red_db),
-                                             socket_connect_timeout=int(redis_timeput),
+                                             socket_connect_timeout=int(redis_timeput), password=red_password,
                              socket_timeout=int(redis_timeput))
         return redis_connection
     except Exception as e:
