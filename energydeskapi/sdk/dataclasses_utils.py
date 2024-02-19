42f3944474aeb8bbd8d2c0e05be47054b680d8eb
steinar <s.r.eriksen@gmail.com>
2024-02-19 09:21:42 +0100

    Helper function to add business days

--- energydeskapi/sdk/dataclasses_utils.py
+++ energydeskapi/sdk/dataclasses_utils.py
@@ -2,6 +2,10 @@ from json import JSONEncoder
 from datetime import date, datetime
 import pandas
 import numpy
+from decimal import Decimal
+from base64 import b64encode, b64decode
+from json import dumps, loads, JSONEncoder
+import pickle
 class DataclassEncoder(JSONEncoder):
     # Override the default method
     def default(self, obj):
@@ -11,9 +15,10 @@ class DataclassEncoder(JSONEncoder):
             return obj.to_dict()
         elif isinstance(obj, numpy.int64):
             return float(obj)
-        elif isinstance(obj, set):
-            return list(obj)
-        print(type(obj))
+        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
+            return super().default(obj)
+        return {'_python_object': b64encode(pickle.dumps(obj)).decode('utf-8')}
+
 def date_hook(json_dict):
     for (key, value) in json_dict.items():
         try:
@@ -21,3 +26,8 @@ def date_hook(json_dict):
         except:
             pass
     return json_dict
+
+def as_python_object(dct):
+    if '_python_object' in dct:
+        return pickle.loads(b64decode(dct['_python_object'].encode('utf-8')))
+    return dct
