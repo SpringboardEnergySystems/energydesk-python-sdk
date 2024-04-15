5335e83a4fce1d4ff77d2c3748c7e88f9467fde7
steinar <s.r.eriksen@gmail.com>
2024-04-15 10:36:22 +0200

    Adding Kafka queue for scheduled tasks

--- energydeskapi/types/event_types.py
+++ energydeskapi/types/event_types.py
@@ -7,6 +7,7 @@ class KafkaQueues(Enum):
     NORDICPOWER_PRODUCTS = "marketdata.nordicpower.products"
     NORDICPOWER_OWNDEALS = "marketdata.nordicpower.owndeals"
     PRODUCTVIEWS = "portfolioview.productview"
+    SCHEDULER_EVENT = "system.schedulerevent"  # General Internal Event . Payload with more info
 
 class MqttTopics(Enum):
     NORDICPOWER_PRICES = "/marketdata/nordicpower/prices"
