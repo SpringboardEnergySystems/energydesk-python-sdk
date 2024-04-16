47ed3abe5b689ab7a41ba7f11aa813de5cdb5d5a
steinar <s.r.eriksen@gmail.com>
2024-04-16 14:37:15 +0200

    Adding Kafka queue for scheduled tasks

--- energydeskapi/flexibility/flexibility_api.py
+++ energydeskapi/flexibility/flexibility_api.py
@@ -104,6 +104,7 @@ class FlexibilityApi:
         logger.debug("Upserting market offering")
         payload = external_market_asset.get_dict(api_connection)
         key = int(payload['pk'])
+        logger.info("Saving external market repr key= {} data= {}".format(key, payload))
         if key > 0:
             success, returned_data, status_code, error_msg = api_connection.exec_patch_url(
                 '/api/flexiblepower/assetsofferedinmarkets/' + str(key) + "/", payload)
