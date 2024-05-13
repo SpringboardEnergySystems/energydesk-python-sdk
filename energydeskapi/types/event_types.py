f56ccf5eaa6d62645a4463d276b82e786baa6b0b
steinar <s.r.eriksen@gmail.com>
2024-05-13 15:32:07 +0200

    Updating settlement samples

--- energydeskapi/types/event_types.py
+++ energydeskapi/types/event_types.py
@@ -7,6 +7,7 @@ class KafkaQueues(Enum):
     NORDICPOWER_PRODUCTS = "marketdata.nordicpower.products"
     NORDICPOWER_OWNDEALS = "marketdata.nordicpower.owndeals"
     PRODUCTVIEWS = "portfolioview.productview"
+    PRODUCTVIEWS_SINGLEROW = "portfolioview.productviewsinglerow"  # User directed events on single changes
     SCHEDULER_EVENTS = "system.schedulerevent"  # General Internal Event . Payload with more info
     FLEXIBILITY_SERVICES_EVENTS = "flexibility.service.events"  # General Internal Event . Payload with more info
 
