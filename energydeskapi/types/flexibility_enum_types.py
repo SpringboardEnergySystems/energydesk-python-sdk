194400c0406ab871b3e743939b41d33570b8a29d
steinar <s.r.eriksen@gmail.com>
2024-05-21 12:27:35 +0200

    Getting rid of unnecessary debug

--- energydeskapi/types/flexibility_enum_types.py
+++ energydeskapi/types/flexibility_enum_types.py
@@ -3,12 +3,13 @@ from enum import Enum
 class RegulationTypeEnums(Enum):
     REGULATE_UP = 1
     REGULATE_DOWN = 2
-
+    NO_REGULATION = 3
 
 def regulation_type_description(x):
     return {
         RegulationTypeEnums.REGULATE_UP: "Regulate Up (consumption down)",
         RegulationTypeEnums.REGULATE_DOWN: "Regulate Down (consumption up)"
+        RegulationTypeEnums.NO_REGULATION: "No Regulation"
     }[x]
 
 
