adcb8bfa890a473647f87cb91399f0ef9aac56f8
steinar <s.r.eriksen@gmail.com>
2024-08-01 08:08:14 +0200

    Optimizer samples

--- energydeskapi/examples/optimization_samples.py
+++ energydeskapi/examples/optimization_samples.py
@@ -2,10 +2,12 @@ import logging
 from energydeskapi.sdk.common_utils import init_api
 from energydeskapi.assetdata.assetdata_api import AssetDataApi
 from energydeskapi.energydesk.general_api import GeneralApi
+from datetime import datetime, timedelta, timezone
 from energydeskapi.optimization.battery_optimizer_api import BatteryOptimizationApi, OptimizerInput
 from energydeskapi.optimization.flexibility_optimizer_api import FlexibilityOptimizationApi
 import pendulum
 import pandas as pd
+import pytz
 import json
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(message)s',
@@ -26,10 +28,25 @@ def optimize_maxusage(api_conn):
     peak=peak*0.9  # 90% of current
     print("Optimize under ", peak)
 
-
     #param['period_from'] = str(pendulum.today(tz="Europe/Oslo").subtract(days=100))[0:10]
     param['tot_capacity_limit'] = peak
-    FlexibilityOptimizationApi.optimize_max_usage(api_conn, param)
+    success, json_res, status_code, error_msg=FlexibilityOptimizationApi.optimize_max_usage(api_conn, param)
+    df_original=pd.DataFrame(json_res['original'])
+    peak_original = float(json_res['peak_original'])
+    df_optimized = pd.DataFrame(json_res['optimized'])
+    peak_optimized = float(json_res['peak_optimized'])
+    def prepare_timestamp(df):
+        df.index = df['timestamp']
+        df.index = pd.to_datetime(df.index)
+        active_timezone = pytz.timezone("Europe/Oslo")
+        df.index = df.index.tz_convert(active_timezone)
+        df['timestamp']=df.index
+        return df
+
+    df_optimized = prepare_timestamp(df_optimized)
+    df_original = prepare_timestamp(df_original)
+
+    print(df_optimized)
 
 
 
@@ -44,6 +61,7 @@ def optimize_battery(api_conn):
     #battery_capacity_kwh = serializers.FloatField(help_text=("Battery Capacity KWh"))
     ##battery_charging_power_kw = serializers.FloatField(help_text=("Discharging Power KW"))
     #battery_dicharging_power_kw = serializers.FloatField(help_text=("Discharging Power KW"))
+
     indata.optimize_fromdate=pendulum.today(tz="Europe/Oslo").subtract(days=40)
     indata.optimize_untildate=pendulum.today(tz="Europe/Oslo").subtract(days=33)
     indata.capacity_kwh=100
