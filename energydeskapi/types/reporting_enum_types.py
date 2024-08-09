577f2fc31bd0628355f6c0875e51af17c4faea65
steinar <s.r.eriksen@gmail.com>
2024-08-09 08:29:00 +0200

    Optimizer samples

--- energydeskapi/types/reporting_enum_types.py
+++ energydeskapi/types/reporting_enum_types.py
@@ -1,12 +1,16 @@
 from enum import Enum
 
 class ReportTypeEnum(Enum):
-    PERIOD_VIEW = 1
-    VAR = 2
+    PRODUCT_VIEW = 1
+    PERIOD_VIEW = 2
+    CONTRACT_TIME_SERIES = 3
+    VAR = 4
 
 def reporttype_description(x):
     return {
+        ReportTypeEnum.PRODUCT_VIEW: "Product View",
         ReportTypeEnum.PERIOD_VIEW: "Period View",
+        ReportTypeEnum.CONTRACT_TIME_SERIES: "Contract Time Series",
         ReportTypeEnum.VAR: "Value at Risk"
     }[x]
 
