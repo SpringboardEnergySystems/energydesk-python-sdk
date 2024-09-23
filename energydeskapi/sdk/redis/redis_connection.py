f24b26bc14ed58dacbd3851df2dbdfce1948a95f
aferrandi <ferrandi.andrea@gmail.com>
2024-09-23 09:46:04 +0200

    chore: "Connecting to redis" can be debug

--- energydeskapi/sdk/redis/redis_connection.py
+++ energydeskapi/sdk/redis/redis_connection.py
@@ -1,3 +1,4 @@
+import traceback
 from typing import Union
 
 import redis
@@ -13,11 +14,11 @@ def connect_to_redis() -> Union[redis.StrictRedis, None]:
         red_port = "6379" if not "REDIS_PORT" in env else env.str("REDIS_PORT")
         red_db = "1" if not "REDIS_DB" in env else env.str("REDIS_DB")
         red_password = None if not "REDIS_PASSWORD" in env else env.str("REDIS_PASSWORD")
-        logger.info("Connecting to redis " + str(red_host) + ":" + str(red_port) + " db" + str(red_db))
+        logger.debug(f"Connecting to redis {red_host}:{red_port} db{red_db}")
         redis_connection = redis.StrictRedis(host=red_host, port=int(red_port), db=int(red_db),
                                              socket_connect_timeout=int(redis_timeput), password=red_password,
                              socket_timeout=int(redis_timeput))
         return redis_connection
     except Exception as e:
-        logging.warning("Could not connect to REDIS  " + str(e))
+        logging.warning(f"Could not connect to REDIS {e}. Error: {traceback.format_exc()}")
     return None
