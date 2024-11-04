b53f4cbfab43f586ec0f7a58a9cab4c2cd6a49cf
steinar <s.r.eriksen@gmail.com>
2024-11-04 16:24:09 +0100

    Changing Text on REGISTERED

--- energydeskapi/examples/flexprovider/registration.py
+++ energydeskapi/examples/flexprovider/registration.py
@@ -157,14 +157,14 @@ if __name__ == '__main__':
     #print(data)
     # FlexibilityApi.remove_asset_flexibility(api_conn, extern_asset_id="Elkjele A")
     #data=FlexibilityApi.get_asset_flexibility_periodoffers(api_conn, parameters={'flexible_asset__asset__extern_asset_id':"Elkjele A"})#,
-    data=FlexibilityApi.get_asset_flexibility_periodoffers(api_conn, parameters={'flexible_asset__asset__extern_asset_id':"3b964bcc-d08c-40c0-b945-3788162b5d59"})
+    #data=FlexibilityApi.get_asset_flexibility_periodoffers(api_conn, parameters={'flexible_asset__asset__extern_asset_id':"3b964bcc-d08c-40c0-b945-3788162b5d59"})
     # FlexibilityApi.remove_asset_flexibility_periodoffers(api_conn, extern_asset_id="Elkjele A")
-    print(data)
-    sys.exit(0)
+    #print(data)
+    #sys.exit(0)
     df=load_assets_from_file("./assets_sample.xlsx")
     for index, row in df.iterrows():
-        register_flexible_asset(api_conn, row)
-        #sys.exit(0)
+        #register_flexible_asset(api_conn, row)
+        sys.exit(0)
         # Sets hours when KW available
         start_hour=random.randint(6, 10)
         end_hour = random.randint(12, 20)
