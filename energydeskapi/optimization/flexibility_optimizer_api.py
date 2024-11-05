a57fefdbffdea3c21707a9ab98ac01380c3deaba
steinar <s.r.eriksen@gmail.com>
2024-11-05 11:27:55 +0100

    Changing Text on REGISTERED

--- energydeskapi/optimization/flexibility_optimizer_api.py
+++ energydeskapi/optimization/flexibility_optimizer_api.py
@@ -11,8 +11,15 @@ class FlexibilityOptimizationApi:
     """
 
     @staticmethod
+    def optimization_price_data(api_connection, parameters):
+        json_res = api_connection.exec_get_url('/api/flexoptimizer/optimpricedata/', parameters)
+        if json_res is None:
+            return None
+        return json_res
+
+    @staticmethod
     def optimize_max_usage(api_connection, parameters):
-        success, json_res, status_code, error_msg = api_connection.exec_post_url('/api/flexoptimizer/optimizemaxusage/', parameters)
+        success, json_res, status_code, error_msg = api_connection.exec('/api/flexoptimizer/optimizemaxusage/', parameters)
         if json_res is None:
             logger.error("Problems optimizing battery " + str(error_msg))
         else:
