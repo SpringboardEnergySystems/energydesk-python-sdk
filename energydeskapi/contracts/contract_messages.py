24a720a71740390fb377dbd05d5fc5c65f5ca70f
aferrandi <ferrandi.andrea@gmail.com>
2024-11-07 16:43:04 +0100

    EDSK-1238: created special contract structures for the live view messages because of problems with JSON conversions.

--- energydeskapi/contracts/contract_messages.py
+++ energydeskapi/contracts/contract_messages.py
@@ -1,8 +1,21 @@
+from dataclasses import dataclass
 from typing import Optional
 
-from energydeskapi.contracts.contracts_api import Contract
+from energydeskapi.types.contract_enum_types import QuantityTypeEnum
 
 
+@dataclass(frozen=True)
+class ContractForLiveViewMessage:
+    trading_book_id: int
+    ticker: str
+    quantity: float
+    volume: float
+    contract_price: float
+    quentity_type: QuantityTypeEnum
+    buy_or_sell: str #"BUY" or "SELL"
+
+@dataclass(frozen=True)
 class ContractChangeMessage:
-    contract: Contract
-    replaced_contract: Optional[Contract]
+    contract: ContractForLiveViewMessage
+    replaced_contract: Optional[ContractForLiveViewMessage]
+
