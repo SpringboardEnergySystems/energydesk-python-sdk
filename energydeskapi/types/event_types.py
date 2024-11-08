9d8b0af2ec5fc7d6c974a499bdb983adadfd2fb5
aferrandi <ferrandi.andrea@gmail.com>
2024-11-08 14:07:40 +0100

    EDSK-1238: contracts, not coontracts.

--- energydeskapi/types/event_types.py
+++ energydeskapi/types/event_types.py
@@ -8,7 +8,7 @@ class KafkaQueues(Enum):
     NORDICPOWER_OWNDEALS = "marketdata.nordicpower.owndeals"
     PRODUCTVIEWS = "portfolioview.productview"
     APPSERVER_CLEARED_CONTRACTS = "energydeskservices.contracts.clearedcontracts"
-    APPSERVER_CONTRACTS_CHANGES = "energydeskservices.contracts.coontractschanges"
+    APPSERVER_CONTRACTS_CHANGES = "energydeskservices.contracts.contractschanges"
     PRODUCTVIEWS_SINGLEROW = "portfolioview.productviewsinglerow"  # User directed events on single changes
     SCHEDULER_EVENTS = "system.schedulerevent"  # General Internal Event . Payload with more info
     FLEXIBILITY_SERVICES_EVENTS = "flexibility.service.events"  # General Internal Event . Payload with more info
