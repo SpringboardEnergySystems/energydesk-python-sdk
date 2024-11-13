f5e08c1f48f10caee8e10eaaf9571678071097a3
aferrandi <ferrandi.andrea@gmail.com>
2024-11-13 13:35:23 +0100

    EDSK-1238: more fields in ContractForLiveViewMessage

--- energydeskapi/contracts/contract_messages.py
+++ energydeskapi/contracts/contract_messages.py
@@ -12,6 +12,12 @@ class ContractForLiveViewMessage:
     id: int
     trading_book_id: int
     ticker: str
+    market: str
+    area: str
+    instrument_type: str
+    delivery_from: str
+    delivery_until: str
+    hours: int
     quantity: float
     volume: float
     contract_price: PriceWithCurrency
