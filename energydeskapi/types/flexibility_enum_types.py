c6230a18e1d0e259e7fa3f6813e716f434b059b3
steinar <s.r.eriksen@gmail.com>
2024-12-17 10:22:01 +0100

    Changing Text on REGISTERED

--- energydeskapi/types/flexibility_enum_types.py
+++ energydeskapi/types/flexibility_enum_types.py
@@ -60,7 +60,7 @@ def portfolio_status_type_description(x):
     return {
         PortfolioStatusTypeEnums.DISABLED: "Portfolio is not in use",
         PortfolioStatusTypeEnums.IDLE: "Portfolio is Idle",
-        PortfolioStatusTypeEnums.ARMED: "Portfolio is Armed"
+        PortfolioStatusTypeEnums.ARMED: "Portfolio is Armed",
         PortfolioStatusTypeEnums.ACTIVATED: "Portfolio is under Activation"
     }[x]
 
