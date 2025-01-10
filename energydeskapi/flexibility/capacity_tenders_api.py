8f67e9c2df5a13568080bdc86c1e19ca323128a6
steinar <s.r.eriksen@gmail.com>
2025-01-10 12:09:51 +0100

    Changing Text on REGISTERED

--- energydeskapi/flexibility/capacity_tenders_api.py
+++ energydeskapi/flexibility/capacity_tenders_api.py
@@ -2,6 +2,7 @@ import logging
 from energydeskapi.assets.assets_api import AssetsApi
 import pandas as pd
 from energydeskapi.grid.grid_api import GridNodeApi
+from energydeskapi.contracts.contracts_api import ContractsApi
 logger = logging.getLogger(__name__)
 
 
@@ -10,7 +11,7 @@ class CapacityTender:
     def __init__(self):
         self.pk = 0
         self.gridnode_asset = 0
-        self.requested_profile=0
+        self.requested_profile={}
         self.quantity_requested=0
         self.quantity_type=0
         self.quantity_unit=0
@@ -26,8 +27,10 @@ class CapacityTender:
         if self.gridnode_asset is not None: dict['gridnode_asset'] = GridNodeApi.get_grid_node_url(api_connection, self.gridnode_asset)
         if self.requested_profile is not None: dict['requested_profile'] = self.requested_profile
         if self.quantity_requested is not None: dict['quantity_requested'] = self.quantity_requested
-        if self.quantity_type is not None: dict['quantity_type'] = self.quantity_type
-        if self.quantity_unit is not None: dict['quantity_unit'] = self.quantity_unit
+        if self.quantity_unit is not None: dict['quantity_unit'] = ContractsApi.get_quantity_unit_url(api_connection,
+                                                                                                            self.quantity_unit)
+        if self.quantity_type is not None: dict['quantity_type'] = ContractsApi.get_quantity_type_url(api_connection,
+                                                                                                            self.quantity_type)
         if self.availability_price is not None: dict['availability_price'] = self.availability_price
         if self.activation_price is not None: dict['activation_price'] = self.activation_price
         if self.price_currency is not None: dict['price_currency'] = self.price_currency
