1420b8a88a97570630c5d90af5a964ad6914f125
steinar <s.r.eriksen@gmail.com>
2025-02-04 13:49:00 +0100

    Added baseline to metering

--- energydeskapi/assetdata/assetdata_api.py
+++ energydeskapi/assetdata/assetdata_api.py
@@ -370,6 +370,20 @@ class AssetDataApi:
         if json_res is not None:
             return json_res
         return None
+
+    @staticmethod
+    def load_grouped_meterdata(api_connection, period_from, period_until, resolution, asset_pk_list=[]):
+        payload={
+            'assets':asset_pk_list,
+            'period_from': period_from,
+            'period_until': period_until,
+            'resolution': resolution
+        }
+        success, returned_data, status_code, error_msg = api_connection.exec_post_url(
+            '/api/assetdata/loadgroupedmeterdata/', payload)
+        return success, returned_data, status_code, error_msg
+
+
     @staticmethod
     def get_assetgroup_timeseries(api_connection,assets, timseries_types=TimeSeriesTypesEnum.FORECASTS, reso=PeriodResolutionEnum.MONTHLY):
         """Fetches forecast for asset group
