55cc6a630f1a15c60d42e093c2fed2ec5cb10adb
aferrandi <ferrandi.andrea@gmail.com>
2025-02-21 11:54:46 +0100

    EDSK-1511: get_deal_data, not getdealdata

--- energydeskapi/conversions/clearinggateway_api.py
+++ energydeskapi/conversions/clearinggateway_api.py
@@ -13,7 +13,7 @@ class ClearingGatewayApi:
     @staticmethod
     def exec_fetch_deal_data() -> list[dict[str,any]]:
         env = environ.Env()
-        server_url = None if 'ELVIZ_PROXY' not in env else f"{env.str('ELVIZ_PROXY')}/clearinggateway/api/clearinggateway/getdealdata"
+        server_url = None if 'ELVIZ_PROXY' not in env else f"{env.str('ELVIZ_PROXY')}/clearinggateway/api/clearinggateway/get_deal_data"
         logger.info(f"Calling clearing gateway URL {server_url}")
         h = {'Authorization': 'Bearer', 'Accept': 'application/json'}
         authsess = ElvizLinksApi.obtain_session()
