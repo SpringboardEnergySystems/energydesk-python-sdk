c14aaabaac591f961553c0b5b27cd7ba4a439981
aferrandi <ferrandi.andrea@gmail.com>
2025-05-28 08:46:44 +0200

    chore: added MontelApi.exec_get_historical()

--- energydeskapi/conversions/montel_api.py
+++ energydeskapi/conversions/montel_api.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from dataclasses import dataclass
+from typing import Optional
 
 import environ
 
@@ -12,12 +13,25 @@ logger = logging.getLogger(__name__)
 class MontelApi:
     @staticmethod
     def exec_reload_products() -> str:
-        env = environ.Env()
-        server_url = None if 'ELVIZ_PROXY' not in env else f"{env.str('ELVIZ_PROXY')}/montel/api/montelservice/reload-products"
+        server_url = MontelApi._build_server_url("reload-products")
         logger.info(f"Calling montel service URL {server_url}")
         h = {'Authorization': 'Bearer', 'Accept': 'application/json'}
         authsess = ElvizLinksApi.obtain_session()
         response = authsess.post(server_url, headers=h)
         return response.text
+
+    @staticmethod
+    def exec_get_historical() -> dict:
+        server_url = MontelApi._build_server_url("historical")
+        logger.info(f"Calling montel service URL {server_url}")
+        h = {'Authorization': 'Bearer', 'Accept': 'application/json'}
+        authsess = ElvizLinksApi.obtain_session()
+        response = authsess.get(server_url, headers=h)
+        return response.json()
+
+    @staticmethod
+    def _build_server_url(path: str) -> Optional[str]:
+        env = environ.Env()
+        return None if 'ELVIZ_PROXY' not in env else f"{env.str('ELVIZ_PROXY')}/montel/api/montelservice/{path}"
 
 
