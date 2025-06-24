367f84a758086ee829e5de0dbcd2b91125046b63
aferrandi <ferrandi.andrea@gmail.com>
2025-06-24 14:29:03 +0200

    EDSK-1795: added approve_all_reconciled_contracts_in_multileg

--- energydeskapi/clearing/clearing_api.py
+++ energydeskapi/clearing/clearing_api.py
@@ -232,7 +232,7 @@ class ClearingApi:
         return success, returned_data, status_code, error_msg
 
     @staticmethod
-    def approve_all_reconciled_contracts_in_multileg(api_connection,date: str, internal_ids: list[int]):
+    def approve_all_reconciled_contracts_in_multileg(api_connection, date: str, internal_ids: list[int]):
         success, returned_data, status_code, error_msg = api_connection.exec_post_url('/api/clearing/approve-reconciled-contracts-in-multileg/',{'date':date, 'internal_ids': internal_ids})
         return success, returned_data, status_code, error_msg
 
