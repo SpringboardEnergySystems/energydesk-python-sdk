2373a803e88f33975063a98e648bd0fe9b9fa859
aferrandi <ferrandi.andrea@gmail.com>
2025-07-02 14:38:01 +0200

    chore: try to fix setup_service_logging

--- energydeskapi/sdk/logging_utils.py
+++ energydeskapi/sdk/logging_utils.py
@@ -49,20 +49,36 @@ def get_loglevel_to_str(level):
 def setup_service_logging(servicetag: str, file_level=logging.WARNING, console_level=logging.INFO):
     console_level=get_loglevel_from_str(get_environment_value("OVERRIDE_CONSOLE_LOGLEVEL", get_loglevel_to_str(console_level)))
     file_level=get_loglevel_from_str(get_environment_value("OVERRIDE_FILE_LOGLEVEL", get_loglevel_to_str(file_level)))
-    smallest_level=console_level if console_level<file_level else file_level
+
+    def create_console_handler():
+        console = logging.StreamHandler()
+        formatter_console = logging.Formatter(get_consolelog_format())
+        console.setFormatter(formatter_console)
+        console.setLevel(console_level)
+        return console
+
+    def create_file_handler():
+        filelogger = TimedRotatingFileHandler('./logs/' + servicetag + '.log', 'midnight', 1)
+        filelogger.setLevel(file_level)
+        formatter_file = logging.Formatter(get_logfile_format(servicetag))
+        filelogger.setFormatter(formatter_file)
+        return filelogger
+
+    def remove_exisiting_handlers():
+        if logging.root is not None and logging.root.handlers is not None:
+            for handler in logging.root.handlers:
+                logging.root.removeHandler(handler)
+
     try:
         os.mkdir("./logs")
     except:
         pass
-    filelogger = TimedRotatingFileHandler('./logs/' + servicetag + '.log', 'midnight', 1)
-    filelogger.setLevel(file_level)
-    formatter_file = logging.Formatter(get_logfile_format(servicetag))
-    filelogger.setFormatter(formatter_file)
-    # set up logging to console
-    console = logging.StreamHandler()
+    filelogger = create_file_handler()
+    console = create_console_handler()
+    remove_exisiting_handlers()
+    logging.basicConfig(handlers=[console, filelogger])
 
-    # set a format which is simpler for console use
-    formatter_console = logging.Formatter(get_consolelog_format())
-    console.setFormatter(formatter_console)
-    console.setLevel(console_level)
-    logging.basicConfig(level=smallest_level, format=get_consolelog_format(), handlers=[console, filelogger])
+
+
+
+
