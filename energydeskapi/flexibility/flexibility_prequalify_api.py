1036e5676469b94fb0080b32a4f197f2c15a3523
steinar <s.r.eriksen@gmail.com>
2025-07-03 07:47:43 +0200

    Fixing longflex parser

--- energydeskapi/flexibility/flexibility_prequalify_api.py
+++ energydeskapi/flexibility/flexibility_prequalify_api.py
@@ -96,6 +96,13 @@ class FlexibilityPrequalifyApi:
         return success, returned_data, status_code, error_msg
 
     @staticmethod
+    def process_offer(api_conn, product_offer_id):
+        payload = {'product_offer_id': product_offer_id }
+        success, returned_data, status_code, error_msg = api_conn.exec_post_url(
+            '/api/flexibility/prequalification/processoffer/', payload)
+        return success, returned_data, status_code, error_msg
+
+    @staticmethod
     def upsert_prequal_bidquality(api_connection, data: FlexPrequalBidTest):
         logger.debug("Upserting flex prequalif")
         payload = json.loads(data.json)
