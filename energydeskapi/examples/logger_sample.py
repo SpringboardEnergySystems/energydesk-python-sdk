e791d1d48e339bbf72c34355a5b770e9feb26d05
steinar <s.r.eriksen@gmail.com>
2025-07-08 21:11:47 +0200

    Copying from wring branch

--- energydeskapi/examples/logger_sample.py
+++ energydeskapi/examples/logger_sample.py
@@ -7,7 +7,7 @@ import logging
 
 if __name__ == '__main__':
     #pd.set_option('display.max_rows', None)
-    cf=LogstashConfig("192.168.1.72", 5000, "energydesk", "dev")
+    cf=LogstashConfig("elk.energydesk.no", 5000, "energydesk", "dev")
     setup_service_logging("heinz",enable_logstash_conf= cf)
     logger = logging.getLogger("heinz")
     logger.info("Hei på deg")
