4c8b0e444c176c5ed5fb43622fb03327696c6591
aferrandi <ferrandi.andrea@gmail.com>
2025-08-22 12:53:31 +0200

    chore: added from_semicolon_colon_config_to_dict() function

---
+++ energydeskapi/sdk/test_env_utils.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from energydeskapi.sdk.env_utils import from_semicolon_colon_config_to_dict
+
+
+class TestEnvUtils(TestCase):
+    def test_from_semicolon_colon_config_to_dict(self):
+        dc = from_semicolon_colon_config_to_dict("interval_ms:10;chunk_size:50;chunk_pause_ms:1000")
+        self.assertDictEqual(dc, {
+            "interval_ms": "10",
+            "chunk_size": "50",
+            "chunk_pause_ms": "1000"
+        })
