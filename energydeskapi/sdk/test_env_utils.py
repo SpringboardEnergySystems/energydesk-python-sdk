4cbfcad25c0e752bc4fb12d8eb87f8909f9abd84
aferrandi <ferrandi.andrea@gmail.com>
2025-08-22 13:10:36 +0200

    chore: tested from_semicolon_colon_config_to_dict() a little more

--- energydeskapi/sdk/test_env_utils.py
+++ energydeskapi/sdk/test_env_utils.py
@@ -11,3 +11,9 @@ class TestEnvUtils(TestCase):
             "chunk_size": "50",
             "chunk_pause_ms": "1000"
         })
+
+    def test_from_semicolon_colon_config_to_dict_wrong(self):
+        self.assertRaises(Exception, from_semicolon_colon_config_to_dict, "interval_ms;chunk_size")
+
+    def test_from_semicolon_colon_config_to_dict_empty(self):
+        dc = from_semicolon_colon_config_to_dict("")
