c964d3ea146c802782a7a4acb67170f4b81aae3b
steinar <s.r.eriksen@gmail.com>
2025-09-24 15:02:39 +0200

    OTC flag on market place

---
+++ energydeskapi/sdk/logstash_singleline_formatter.py
@@ -0,0 +1,17 @@
+import json
+from logstash_async.formatter import LogstashFormatter
+
+class SingleLineLogstashFormatter(LogstashFormatter):
+    def _format_to_dict(self, record):
+        message = super()._format_to_dict(record)
+        # Add tags if present in the record
+        tags = getattr(record, 'tags', None)
+        if tags is not None:
+            message['tags'] = tags
+        return message
+
+    def format(self, record):
+        # Remove newlines from the message
+        if hasattr(record, 'msg'):
+            record.msg = str(record.msg).replace('\n', '\\n').replace('\r', '\\r')
+        return super().format(record)
