b35a0ff06ac81f0a202dcf4235d9e3aedb812350
steinar <steinar@springboard.energy>
2025-11-10 07:49:29 +0100

    Better checking environment

--- energydeskapi/sdk/logging_utils.py
+++ energydeskapi/sdk/logging_utils.py
@@ -128,7 +128,7 @@ def setup_service_logging(servicetag: str, file_level: int=logging.INFO, console
 # Just to make setup simpler with some standardized env names
 def create_logstash_from_environment():
     env=environ.Env()
-    enabled = False if "LOGSTASH_ENABLED" not in env else env.bool("LOGSTASH_ENABLED")
+    enabled = False if "LOGSTASH_ENABLED" not in env else env.str("LOGSTASH_ENABLED").upper() == "TRUE"
     if enabled:
         host = None if "LOGSTASH_HOST" not in env else env.str("LOGSTASH_HOST")
         port = None if "LOGSTASH_PORT" not in env else env.int("LOGSTASH_PORT")
