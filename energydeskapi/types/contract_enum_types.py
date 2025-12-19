437420efc6391f0a4e1b0d0432ce58e61e5155aa
aferrandi <ferrandi.andrea@gmail.com>
2025-12-19 09:39:31 +0100

    chore: renamed COMMISSION_FEE to CLEARING_COMMISSION_FEE

--- energydeskapi/types/contract_enum_types.py
+++ energydeskapi/types/contract_enum_types.py
@@ -56,7 +56,7 @@ class FeeTypeEnum(Enum):
     TRADING_FEE = 1
     CLEARING_FEE = 2
     BROKER_FEE = 3
-    COMMISSION_FEE = 4
+    CLEARING_COMMISSION_FEE = 4
 
 
 def fee_type_description(x):
@@ -64,7 +64,7 @@ def fee_type_description(x):
         FeeTypeEnum.TRADING_FEE: "Trading Fee",
         FeeTypeEnum.CLEARING_FEE: "Clearing Fee",
         FeeTypeEnum.BROKER_FEE: "Broker Fee",
-        FeeTypeEnum.COMMISSION_FEE: "Commission Fee"
+        FeeTypeEnum.CLEARING_COMMISSION_FEE: "Clearing Commission Fee"
     }[x]
 
 def quantity_type_description(x):
