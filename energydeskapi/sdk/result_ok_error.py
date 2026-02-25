b0400fed9747e86773e48c75156eb8b54f7e1efe
aferrandi <ferrandi.andrea@gmail.com>
2026-02-25 15:16:42 +0100

    EN-2028:  fixed is_ok field so that it is a property

--- energydeskapi/sdk/result_ok_error.py
+++ energydeskapi/sdk/result_ok_error.py
@@ -7,12 +7,19 @@ E = TypeVar("E")  # The type of the error message/exception
 @dataclass(frozen=True)
 class Ok(Generic[T]):
     value: T
-    is_ok: bool = True
+
+    @property
+    def is_ok(self) -> bool:
+        return True
 
 @dataclass(frozen=True)
 class Error(Generic[E]):
     error: E
-    is_ok: bool = False
+
+    @property
+    def is_ok(self) -> bool:
+        return False
 
 # A type alias for easier usage
 Result = Union[Ok[T], Error[E]]
+
