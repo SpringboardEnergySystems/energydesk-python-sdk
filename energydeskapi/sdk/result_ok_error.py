a67d5f055b57b6ebd6b85bfa570daf3bda296342
aferrandi <ferrandi.andrea@gmail.com>
2026-02-25 14:07:31 +0100

    chore: added a Result class with ok and error variations (Either/Maybe)

---
+++ energydeskapi/sdk/result_ok_error.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from typing import TypeVar, Generic, Union
+
+T = TypeVar("T")  # The type of the success value
+E = TypeVar("E")  # The type of the error message/exception
+
+@dataclass(frozen=True)
+class Ok(Generic[T]):
+    value: T
+    is_ok: bool = True
+
+@dataclass(frozen=True)
+class Err(Generic[E]):
+    error: E
+    is_ok: bool = False
+
+# A type alias for easier usage
+Result = Union[Ok[T], Err[E]]
