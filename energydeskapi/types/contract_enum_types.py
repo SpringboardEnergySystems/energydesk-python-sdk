fd6ff1b0b8617797c430d17180f5145dd9c9407a
vnbenny <info@vnbenny.com>
2026-03-09 13:14:36 +0100

    Sorry typo!

--- energydeskapi/types/contract_enum_types.py
+++ energydeskapi/types/contract_enum_types.py
@@ -129,7 +129,7 @@ def contract_type_description(x):
         ContractTypeEnum.POSITION_TRANSFER: "Position Transfer",
         ContractTypeEnum.INTERNAL: "Internal Contract",
         ContractTypeEnum.FLEX_ACTIVATION: "Flex Activation",
-        ContractTypeEnum.EURONEX: "Euronext"
+        ContractTypeEnum.EURONEXT: "Euronext"
     }[x]
 
 def contract_status_description(x):
