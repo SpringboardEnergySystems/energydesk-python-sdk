31aa6726ec7b2934628225040eb23b365bda5e68
aferrandi <ferrandi.andrea@gmail.com>
2026-04-23 17:02:29 +0200

    chore: __str__ methods for DatePeriod, DatetimePeriod and DatetimePeriodQuery

--- energydeskapi/sdk/date_period.py
+++ energydeskapi/sdk/date_period.py
@@ -7,15 +7,20 @@ from typing import Optional
 class DatePeriod:
     from_date: date
     until_date: date
-
+    def __str__(self):
+        return f"DatePeriod{self.from_date.isoformat()},{self.until_date.isoformat()}"
 
 @dataclass(frozen=True)
 class DatetimePeriod:
     from_datetime: datetime
     until_datetime: datetime
+    def __str__(self):
+        return f"DatetimePeriod{self.from_datetime.isoformat()},{self.until_datetime.isoformat()}"
 
 
 @dataclass(frozen=True)
 class DatetimePeriodQuery:
     from_datetime: Optional[datetime]
     until_datetime: Optional[datetime]
+    def __str__(self):
+        return f"DatetimePeriodQuery{self.from_datetime.isoformat() if self.from_datetime is not None else ''},{self.until_datetime.isoformat() if self.until_datetime is not None else ''}"
